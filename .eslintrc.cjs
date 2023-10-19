bcee40088a15fa587c288d67bf41e4ffc9ba430a
이민희 <mhl98@dgu.ac.kr>
2023-10-19 18:13:19 +0900

    chore: import/order 에러 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'import/order': [
