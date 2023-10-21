55aa908c0108b062dd3ea53bded9152dbed7b485
backward99 <wjeeho@naver.com>
2023-10-21 14:50:01 +0900

    feat: 라우터 설정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,26 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '~/App';
+import { AdminPage } from '~/pages/AdminPage';
+import { ApplyEventPage, CreateEventPage, EventDetailPage, EventListPage } from '~/pages/EventPage';
+import { MainPage } from '~/pages/MainPage';
+import { MenteeMyPage, MentorMyPage, WriteReviewPage } from '~/pages/MyPage';
+import { MenteeEditProfilePage, MentorEditProfilePage } from '~/pages/ProfilePage';
 import {
-  AdminPage,
-  ApplyEventPage,
-  CreateEventPage,
   CreateResumePage,
-  EditProfilePage,
-  EventListPage,
-  InputBasicInfoPage,
-  MainPage,
-  MenteeMyPage,
-  MenteeResumeDetailPage,
-  MenteeSignupPage,
-  MenteeViewEventPage,
-  MentorMyPage,
-  MentorResumeDetailPage,
-  MentorSignupPage,
-  MentorViewEventPage,
-  RequestedResumePage,
+  EditResumePage,
+  ResumeDetailPage,
   ReviewResumePage,
-  WriteReviewPage,
-} from '~/pages';
+} from '~/pages/ResumePage';
+import { SignInPage } from '~/pages/SignInPage';
+import { CommonSignUpPage, MenteeSignUpPage, MentorSignUpPage } from '~/pages/SignUpPage';
 
 const router = createBrowserRouter([
   {
@@ -30,25 +22,24 @@ const router = createBrowserRouter([
       { index: true, element: <MainPage /> },
       { path: 'mypage/mentee', element: <MenteeMyPage /> },
       { path: 'mypage/mentor', element: <MentorMyPage /> },
-      { path: 'user/edit-info', element: <EditProfilePage /> },
+      { path: 'user/edit-info/mentee', element: <MenteeEditProfilePage /> },
+      { path: 'user/edit-info/mentor', element: <MentorEditProfilePage /> },
 
       { path: 'resume/create', element: <CreateResumePage /> },
-      { path: 'resume/edit/:id', element: <EditProfilePage /> },
+      { path: 'resume/edit/:id', element: <EditResumePage /> },
       { path: 'resume/review/:id', element: <ReviewResumePage /> },
-      { path: 'resume/detail/mentee/:id', element: <MenteeResumeDetailPage /> },
-      { path: 'resume/detail/mentor/:id', element: <MentorResumeDetailPage /> },
-      { path: 'requested-resume', element: <RequestedResumePage /> },
+      { path: 'resume/detail/:id', element: <ResumeDetailPage /> },
       { path: 'write-review', element: <WriteReviewPage /> },
 
       { path: 'event/create', element: <CreateEventPage /> },
       { path: 'event/view', element: <EventListPage /> },
-      { path: 'event/view/mentee/:eventId', element: <MenteeViewEventPage /> },
-      { path: 'event/view/mentor/:eventId', element: <MentorViewEventPage /> },
+      { path: 'event/view/:eventId', element: <EventDetailPage /> },
       { path: 'event/apply', element: <ApplyEventPage /> },
 
-      { path: 'input-basic-info', element: <InputBasicInfoPage /> },
-      { path: 'signup/mentee', element: <MenteeSignupPage /> },
-      { path: 'signup/mentor', element: <MentorSignupPage /> },
+      { path: 'sign-up/common', element: <CommonSignUpPage /> },
+      { path: 'sign-up/mentee', element: <MenteeSignUpPage /> },
+      { path: 'sign-up/mentor', element: <MentorSignUpPage /> },
+      { path: 'sign-in', element: <SignInPage /> },
 
       { path: 'admin', element: <AdminPage /> },
     ],
