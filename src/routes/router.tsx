00a45ee81c25d3b9b4ce30ebf329b152a5b3dba4
backward99 <wjeeho@naver.com>
2023-10-23 10:59:57 +0900

    fix: 라우팅 변경, 파일 이름 변경

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '~/App';
-import { AdminPage } from '~/pages/AdminPage';
-import { ApplyEventPage, CreateEventPage, EventDetailPage, EventListPage } from '~/pages/EventPage';
-import { MainPage } from '~/pages/MainPage';
-import { MenteeMyPage, MentorMyPage, WriteReviewPage } from '~/pages/MyPage';
-import { MenteeEditProfilePage, MentorEditProfilePage } from '~/pages/ProfilePage';
-import {
-  CreateResumePage,
-  EditResumePage,
-  ResumeDetailPage,
-  ReviewResumePage,
-} from '~/pages/ResumePage';
-import { SignInPage } from '~/pages/SignInPage';
-import { CommonSignUpPage, MenteeSignUpPage, MentorSignUpPage } from '~/pages/SignUpPage';
+import AdminPage from '~/pages/AdminPage/AdminPage';
+import ApplyEventPage from '~/pages/ApplyEventPage/ApplyEventPage';
+import CreateEventPage from '~/pages/EventPage/CreateEventPage';
+import EventDetailPage from '~/pages/EventPage/EventDetailPage';
+import EventListPage from '~/pages/EventPage/EventListPage';
+import MainPage from '~/pages/MainPage/MainPage';
+import MenteeMyPage from '~/pages/MyPage/MenteeMyPage';
+import MentorMyPage from '~/pages/MyPage/MentorMyPage';
+import WriteReviewPage from '~/pages/MyPage/WriteReviewPage';
+import MenteeEditProfilePage from '~/pages/ProfilePage/MenteeEditProfilePage';
+import MentorEditProfilePage from '~/pages/ProfilePage/MentorEditProfilePage';
+import CommentResumePage from '~/pages/ResumePage/CommentResumePage';
+import CreateResumePage from '~/pages/ResumePage/CreateResumePage';
+import EditResumePage from '~/pages/ResumePage/EditResumePage';
+import ResumeDetailPage from '~/pages/ResumePage/ResumeDetailPage';
+import SignInPage from '~/pages/SignInPage/SignInPage';
+import CommonSignUpPage from '~/pages/SignUpPage/CommonSignUpPage';
+import MenteeSignUpPage from '~/pages/SignUpPage/MenteeSignUpPage';
+import MentorSignUpPage from '~/pages/SignUpPage/MentorSignUpPage';
 
 const router = createBrowserRouter([
   {
@@ -26,9 +32,9 @@ const router = createBrowserRouter([
       { path: 'user/edit-info/mentor', element: <MentorEditProfilePage /> },
 
       { path: 'resume/create', element: <CreateResumePage /> },
-      { path: 'resume/edit/:id', element: <EditResumePage /> },
-      { path: 'resume/review/:id', element: <ReviewResumePage /> },
-      { path: 'resume/detail/:id', element: <ResumeDetailPage /> },
+      { path: 'resume/:id/edit', element: <EditResumePage /> },
+      { path: 'resume/:id/comment', element: <CommentResumePage /> },
+      { path: 'resume/:id', element: <ResumeDetailPage /> },
       { path: 'write-review', element: <WriteReviewPage /> },
 
       { path: 'event/create', element: <CreateEventPage /> },
