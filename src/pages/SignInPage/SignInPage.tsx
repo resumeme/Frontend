ca535403688cb45b5fe09dee124c350c059137e4
이민희 <mhl98@dgu.ac.kr>
2023-10-26 01:57:27 +0900

    feat: 로그인 페이지 마크업 추가

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -11,6 +11,8 @@ const SignInPage = () => {
       <div>이력,써에 가입하고 피드백을 받아보세요.</div>
       <div>연동할 소셜 서비스 계정을 선택하세요.</div>
       <button onClick={handleKakaoClick}>카카오 로그인</button>
+      <a href="#">계정을 잊으셨나요?</a>
+      <div>ⓒ 아몬드빼빼로</div>
     </div>
   );
 };
