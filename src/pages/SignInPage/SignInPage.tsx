12ea5a911fef3d8f33fe7b30dcd2db54f067463f
이민희 <mhl98@dgu.ac.kr>
2023-10-26 01:54:07 +0900

    feat: 로그인 페이지 마크업 + 카카오 로그인 페이지 리다이렉션

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,5 +1,18 @@
+import { environments } from '~/config/environments';
+
 const SignInPage = () => {
-  return <></>;
+  const kakaoSignInUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${environments.kakaoRestApiKeyEnv()}&redirect_uri=${environments.kakaoRedirectUriEnv()}&response_type=code`;
+  const handleKakaoClick = () => {
+    window.location.href = kakaoSignInUrl;
+  };
+  return (
+    <div>
+      <div>resume.me</div>
+      <div>이력,써에 가입하고 피드백을 받아보세요.</div>
+      <div>연동할 소셜 서비스 계정을 선택하세요.</div>
+      <button onClick={handleKakaoClick}>카카오 로그인</button>
+    </div>
+  );
 };
 
 export default SignInPage;
