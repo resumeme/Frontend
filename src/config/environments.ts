ee0485026546d2cffdfca8445620e52d6cfa32e3
이민희 <mhl98@dgu.ac.kr>
2023-10-26 01:30:31 +0900

    feat: 카카오 소셜 로그인 key, redirect uri 환경변수 추가

---
+++ src/config/environments.ts
@@ -0,0 +1,13 @@
+const { VITE_KAKAO_REST_API_KEY, VITE_KAKAO_REDIRECT_URI } = import.meta.env;
+
+export const environments = {
+  kakaoRestApiKeyEnv: () => checkEnv(VITE_KAKAO_REST_API_KEY),
+  kakaoRedirectUriEnv: () => checkEnv(VITE_KAKAO_REDIRECT_URI),
+};
+
+const checkEnv = (envKey: string) => {
+  if (!envKey) {
+    throw new Error(`${envKey} 환경변수가 설정돼있지 않습니다.`);
+  }
+  return envKey;
+};
