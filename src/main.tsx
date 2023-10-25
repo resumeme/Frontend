5a41b133543e83366a65c2213b1f6f54347aeeb7
Dongho Shin <kakkiid@gmail.com>
2023-10-25 17:51:01 +0900

    feat: 차크라UI 스타일 시스템 테마 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from '~/routes/router';
+import Fonts from '~/theme/font.tsx';
+import theme from '~/theme/index.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <Fonts />
         <RouterProvider router={router} />
       </ChakraProvider>
     </QueryClientProvider>
---
+++ src/theme/index.tsx
@@ -0,0 +1,82 @@
+import { StyleFunctionProps, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  breakpoints: {
+    base: '0em', // 0px
+    sm: '30em', // ~480px. em is a relative unit and is dependant on the font size.
+    md: '48em', // ~768px
+    lg: '62em', // ~992px
+    xl: '80em', // ~1280px
+  },
+  styles: {
+    // eslint-disable-line no-unused-vars
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        color: 'default',
+        bg: 'bg',
+      },
+    }),
+  },
+  colors: {
+    bg: '#F7F8FA',
+    gray: {
+      100: '#FFFFFF',
+      200: '#F7F8FA',
+      300: '#E5E8EB',
+      400: '#ADB5BD',
+      500: '#8B95A1',
+      600: '#6B7684',
+      700: '#4E5968',
+      800: '#333D4B',
+      900: '#191F28',
+    },
+    primary: {
+      100: '#DDEBE5',
+      200: '#C5E1D3',
+      300: '#B4DECA',
+      400: '#95CDB2',
+      500: '#7DC3A1',
+      600: '#65B890',
+      700: '#4DAF80',
+      800: '#35A46F',
+      900: '#05904D',
+    },
+    highlight: {
+      900: '#FA3D3D',
+      500: '#F89999',
+    },
+  },
+  fonts: {
+    heading: 'Pretendard-Regular',
+    body: 'Pretendard-Regular',
+    mono: 'Pretendard-Regular',
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+  fontSizes: {
+    xs: '0.8rem', // 12px
+    sm: '0.9rem', // 14px
+    md: '1rem', // 16px
+    lg: '1.1rem', // 18px
+    xl: '1.3rem', // 20px
+    '2xl': '1.5rem', // 24px
+    '3xl': '1.8rem', // 28px
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '3.75rem',
+    '7xl': '4.5rem',
+    '8xl': '6rem',
+    '9xl': '8rem',
+  },
+});
+
+export default theme;
