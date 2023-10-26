770d4690b53a783ab7417c3c74b22e0d03fd8919
backward99 <wjeeho@naver.com>
2023-10-26 17:01:03 +0900

    feat: 회원 가입 페이지 - 공통 UI
    react hook form 적용

--- src/pages/SignUpPage/CommonSignUpPage.tsx
+++ src/pages/SignUpPage/CommonSignUpPage.tsx
@@ -1,14 +1,13 @@
-import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/form-control';
 import { CheckIcon } from '@chakra-ui/icons';
 import { Input } from '@chakra-ui/input';
 import { Box, Container } from '@chakra-ui/layout';
-import { Card, CardBody, Image, Stack, HStack } from '@chakra-ui/react';
-import { Flex } from '@chakra-ui/react';
+import { Card, CardBody, Image, Stack, HStack, Button } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 import { Divider } from '@chakra-ui/react';
 import { Step, StepIndicator, Stepper, useSteps } from '@chakra-ui/stepper';
-import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 const CommonSignUpPage = () => {
   const steps = [
@@ -19,12 +18,38 @@ const CommonSignUpPage = () => {
 
   const { activeStep, setActiveStep } = useSteps({ index: 1, count: steps.length });
 
-  //From Control
-  const [input, setInput] = useState('');
+  type FormValues = {
+    name: string;
+    nickName: string;
+    phoneNumber: number;
+  };
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>();
 
-  const handleInputChange = (e) => setInput(e.target.value);
+  const onSubmit: SubmitHandler<FormValues> = (values) => {
+    return new Promise(() => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+      }, 3000);
+    });
+  };
 
-  const isError = input === '';
+  const errorMessage = {
+    name: '이름을 입력해주세요.',
+    nickName: {
+      required: '닉네임을 입력해주세요.',
+      minLength: '2글자 이상 입력해주세요.',
+      maxLength: '10글자 이하로 입력해주세요.',
+    },
+    phoneNumber: {
+      required: '연락처를 입력해주세요.',
+      minLength: '9글자 이상 입력해주세요.',
+      maxLength: '11글자 이하로 입력해주세요.',
+    },
+  };
 
   return (
     <Container
@@ -63,85 +88,112 @@ const CommonSignUpPage = () => {
         </Stepper>
       </Box>
       <Stack>
-        <FormControl isInvalid={isError}>
-          <Flex>
-            <FormLabel>이름</FormLabel>
-            <FormLabel color="05904D">*</FormLabel>
-          </Flex>
-          <Input
-            value={input}
-            onChange={handleInputChange}
-            placeholder="본명을 입력해주세요."
-          />
-        </FormControl>
-        <FormControl
-          isInvalid={isError}
-          isRequired
-        >
-          <FormLabel>Email</FormLabel>
-          <Input
-            value={input}
-            placeholder="닉네임을 2글자 이상 10글자 이하로 작성해주세요."
-            onChange={handleInputChange}
-          />
-        </FormControl>
-        <FormControl
-          isInvalid={isError}
-          isRequired
-        >
-          <FormLabel>연락처</FormLabel>
-          <Input
-            type="number"
-            value={input}
-            onChange={handleInputChange}
-            placeholder="'-'기호 없이 작성해주세요."
-          />
-        </FormControl>
-        <FormControl>
-          <Flex>
-            <FormLabel>역할 선택</FormLabel>
-            <FormLabel color="05904D">*</FormLabel>
-          </Flex>
-        </FormControl>
-        <HStack mx={'auto'}>
-          <Card
-            maxW="10.625rem"
-            my={0}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="name">이름</FormLabel>
+            <Input
+              id="name"
+              placeholder="본명을 입력해주세요."
+              {...register('name', {
+                required: true,
+                pattern: /&s/,
+              })}
+            />
+            {errors?.name && <FormErrorMessage>{errorMessage.name}</FormErrorMessage>}
+          </FormControl>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="nickName">닉네임</FormLabel>
+            <Input
+              id="nickName"
+              placeholder="닉네임을 2글자 이상 10글자 이하로 작성해주세요."
+              {...register('nickName', {
+                required: true,
+                minLength: 2,
+                maxLength: 10,
+              })}
+            />
+
+            {
+              <FormErrorMessage>
+                {errorMessage.nickName[errors.nickName?.type as keyof typeof errorMessage.nickName]}
+              </FormErrorMessage>
+            }
+          </FormControl>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="phoneNumber">연락처</FormLabel>
+            <Input
+              focusBorderColor="primary.900"
+              id="phoneNumber"
+              placeholder="'-'기호 없이 작성해주세요."
+              {...register('phoneNumber', {
+                required: true,
+                minLength: 9,
+                maxLength: 11,
+              })}
+            />
+
+            {
+              <FormErrorMessage>
+                {
+                  errorMessage.phoneNumber[
+                    errors.phoneNumber?.type as keyof typeof errorMessage.phoneNumber
+                  ]
+                }
+              </FormErrorMessage>
+            }
+          </FormControl>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel>역할 선택 *</FormLabel>
+            <HStack mx={'auto'}>
+              <Card
+                maxW="10.625rem"
+                my={0}
+              >
+                <CardBody>
+                  <Stack spacing="3">
+                    <HStack>
+                      <CheckIcon />
+                      <Heading size="md">멘티</Heading>
+                    </HStack>
+                    <Image
+                      src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                      alt="Green double couch with wooden legs"
+                      borderRadius="lg"
+                    />
+                    <Text>이력서를 관리하고, 자유롭게 피드백을 나눌 수 있습니다.</Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+              </Card>
+              <Card maxW="10.625rem">
+                <CardBody>
+                  <Stack spacing="3">
+                    <HStack>
+                      <CheckIcon />
+                      <Heading size="md">멘티</Heading>
+                    </HStack>
+                    <Image
+                      src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                      alt="Green double couch with wooden legs"
+                      borderRadius="lg"
+                    />
+                    <Text>이력서를 관리하고, 자유롭게 피드백을 나눌 수 있습니다.</Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+              </Card>
+            </HStack>
+          </FormControl>
+
+          <Button
+            mt={4}
+            bgColor="primary.900"
+            isLoading={isSubmitting}
+            type="submit"
           >
-            <CardBody>
-              <Stack spacing="3">
-                <HStack>
-                  <CheckIcon />
-                  <Heading size="md">멘티</Heading>
-                </HStack>
-                <Image
-                  src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt="Green double couch with wooden legs"
-                  borderRadius="lg"
-                />
-                <Text>이력서를 관리하고, 자유롭게 피드백을 나눌 수 있습니다.</Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-          <Card maxW="10.625rem">
-            <CardBody>
-              <Stack spacing="3">
-                <HStack>
-                  <CheckIcon />
-                  <Heading size="md">멘티</Heading>
-                </HStack>
-                <Image
-                  src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt="Green double couch with wooden legs"
-                  borderRadius="lg"
-                />
-                <Text>이력서를 관리하고, 자유롭게 피드백을 나눌 수 있습니다.</Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-        </HStack>
+            다음으로
+          </Button>
+        </form>
       </Stack>
     </Container>
   );
