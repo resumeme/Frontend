78357be694b0f45247bc40af575de77d3aab6f5a
이민희 <mhl98@dgu.ac.kr>
2023-10-28 02:02:09 +0900

    fix: border radius theme 이름 수정, 불필요한 기존 theme 제거

--- src/theme/foundations/borderRadius.ts
+++ src/theme/foundations/borderRadius.ts
@@ -1,9 +1,6 @@
-import { theme as defaultTheme } from '@chakra-ui/react';
-
 const borderRadius = {
   radii: {
-    ...defaultTheme.radii,
-    xl: '0.625rem',
+    buttonRadius: '0.625rem',
   },
 };
 
