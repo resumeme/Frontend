fb1945627ff14ad2e225651210d18e5125607cf3
backward99 <wjeeho@naver.com>
2023-10-27 10:10:00 +0900

    feat: FormInput 필수 입력, 정렬 받기

--- src/components/molecules/FormInput/FormInput.tsx
+++ src/components/molecules/FormInput/FormInput.tsx
@@ -1,25 +1,52 @@
-import { Input, FormControl, FormLabel, FormErrorMessage, InputProps } from '@chakra-ui/react';
-import React from 'react';
-import { UseFormRegisterReturn, FieldErrors } from 'react-hook-form';
+import { Input, FormControl, FormLabel, FormErrorMessage, Flex, Box } from '@chakra-ui/react';
+import { FormInputProps } from '~/types/formInput';
 
-type FormInputProps = {
-  id: string;
-  label: string;
-  placeholder: string;
-  register: UseFormRegisterReturn;
-  errors: FieldErrors;
-} & InputProps;
-
-const FormInput: React.FC<FormInputProps> = ({ id, label, placeholder, register, errors }) => {
+const FormInput = ({
+  id,
+  label,
+  placeholder,
+  register,
+  errors,
+  direction = 'column',
+  isRequired = false,
+}: FormInputProps) => {
   return (
-    <FormControl isInvalid={!!errors[id]}>
-      <FormLabel htmlFor={id}>{label}</FormLabel>
-      <Input
-        id={id}
-        placeholder={placeholder}
-        {...register}
-      />
-      <FormErrorMessage>{errors[id]?.message as string}</FormErrorMessage>
+    <FormControl
+      isInvalid={!!errors[id]}
+      w={'100%'}
+    >
+      <Flex direction={direction}>
+        <FormLabel
+          w={'6rem'}
+          htmlFor={id}
+          mx={0}
+          mt={'0.5rem'}
+        >
+          {label}
+          {isRequired && (
+            <Box
+              as="span"
+              color="primary.900"
+            >
+              {' '}
+              *
+            </Box>
+          )}
+        </FormLabel>
+        <Flex
+          w={'100%'}
+          direction={'column'}
+        >
+          <Input
+            w={'100%'}
+            flexGrow={'1'}
+            id={id}
+            placeholder={placeholder}
+            {...register}
+          />
+          <FormErrorMessage>{errors[id]?.message as string}</FormErrorMessage>
+        </Flex>
+      </Flex>
     </FormControl>
   );
 };
