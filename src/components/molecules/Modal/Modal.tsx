c6fe96022fb44db8a2d6a99cab1b5a6c7c2e5433
이민희 <mhl98@dgu.ac.kr>
2023-10-28 13:04:14 +0900

    feat: 공통 모달 컴포넌트 생성

---
+++ src/components/molecules/Modal/Modal.tsx
@@ -0,0 +1,69 @@
+import {
+  Modal as ChakraModal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Button,
+  ModalBodyProps,
+} from '@chakra-ui/react';
+
+type ModalProps = ModalBodyProps & {
+  isOpen: boolean;
+  onClose: () => void;
+  hasCloseButton?: boolean;
+  hasFooter?: boolean;
+  title?: string;
+  children?: React.ReactNode;
+};
+
+const Modal = ({
+  isOpen,
+  onClose,
+  hasCloseButton = true,
+  hasFooter = false,
+  title,
+  children,
+  ...modalBodyProps
+}: ModalProps) => {
+  return (
+    <>
+      <ChakraModal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        size="3xl"
+      >
+        <ModalOverlay bg={'rgba(229, 232, 235, 0.80)'} />
+        <ModalContent
+          borderRadius={'1.5rem'}
+          p={'1.5rem'}
+        >
+          {title && <ModalHeader paddingTop={0}>{title}</ModalHeader>}
+          {hasCloseButton && <ModalCloseButton p={'1.5rem'} />}
+          <ModalBody {...modalBodyProps}>{children}</ModalBody>
+          {hasFooter && (
+            <ModalFooter
+              paddingBottom={0}
+              px={0}
+            >
+              {/*FIXME - 공통 버튼 컴포넌트로 대체하기 */}
+              <Button
+                colorScheme="blue"
+                mr={3}
+                onClick={onClose}
+              >
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          )}
+        </ModalContent>
+      </ChakraModal>
+    </>
+  );
+};
+
+export default Modal;
