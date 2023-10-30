88137ed3095a58c27847b73c9fb5e4b6a484d0d9
backward99 <wjeeho@naver.com>
2023-10-30 22:11:09 +0900

    fix: 문자열 상수로 분리

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,6 +1,78 @@
 import { Box, Divider, Flex, HStack, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
+const FOOTER_CONTENT = {
+  main: {
+    title: 'resumeme',
+  },
+  links: {
+    almondPepero: {
+      text: '아몬드빼빼로',
+      url: 'https://www.notion.so/prgrms/11-0-2a36f4b93e324e3390ed43b831f9f557',
+    },
+    termsOfService: {
+      text: '이용 약관',
+      url: '', // 이용약관 페이지 링크 추가 필요
+    },
+    blog: {
+      text: '블로그',
+      url: '', // 블로그 페이지 링크 추가 필요
+    },
+    privacyPolicy: {
+      text: '개인정보 처리방침',
+      url: '', // 개인정보 처리방침 페이지 링크 추가 필요
+    },
+  },
+  team: {
+    name: '팀 아몬드빼빼로',
+    mentors: '멘토 (BE) 조규현, (FE) 윤지석',
+    backend: '백엔드',
+    backendMembers: ['김범우', '주홍석', '최지연'],
+    frontend: '프론트엔드',
+    frontendMembers: ['신동호', '우지호', '이민희'],
+    address: '서울특별시 프롱구 백둥대로 4기',
+    phoneNumber: '전화번호: 000-0000-0000',
+  },
+  pages: {
+    resume: {
+      text: '이력서',
+      link: {
+        resumeManagement: {
+          text: '이력서 관리',
+          url: '/', // 이력서 관리 페이지 링크 추가 필요
+        },
+      },
+    },
+    event: {
+      text: '이벤트',
+      link: {
+        ongoingEvent: {
+          text: '진행중인 이벤트',
+          url: '/event/view',
+        },
+      },
+    },
+    community: {
+      text: '커뮤니티',
+      link: {
+        mentorCoffeeChat: {
+          text: '멘토 커피챗',
+          url: '/coffee-chat',
+        },
+        freeBoard: {
+          text: '자유게시판',
+          url: '/general',
+        },
+        successfulResumes: {
+          text: '합격 이력서',
+          url: '/resume/references',
+        },
+      },
+    },
+  },
+  copyright: 'ⓒ 2023 이력, 써',
+};
+
 const Footer = () => {
   return (
     <Box
@@ -23,7 +95,7 @@ const Footer = () => {
             mr={'auto'}
             fontWeight={900}
           >
-            resumeme
+            {FOOTER_CONTENT.main.title}
           </Text>
           <HStack
             color={'gray.400'}
@@ -31,30 +103,15 @@ const Footer = () => {
             fontWeight={600}
             spacing={'3.75rem'}
           >
-            <Link
-              to={'https://www.notion.so/prgrms/11-0-2a36f4b93e324e3390ed43b831f9f557'}
-              target="_blank"
-            >
-              <Text>아몬드빼빼로</Text>
-            </Link>
-            <Link //Todo: 이용약관 추가 시 연결
-              to={''}
-              target=""
-            >
-              <Text>이용 약관</Text>
-            </Link>
-            <Link //Todo: 블로그 생성 시 연결
-              to={''}
-              target=""
-            >
-              <Text>블로그</Text>
-            </Link>
-            <Link //Todo: 개인정보 처리방침 추가 후 연결
-              to={''}
-              target=""
-            >
-              <Text>개인정보 처리방침</Text>
-            </Link>
+            {Object.values(FOOTER_CONTENT.links).map((link) => (
+              <Link
+                key={link.text}
+                to={link.url}
+                target="_blank"
+              >
+                <Text>{link.text}</Text>
+              </Link>
+            ))}
           </HStack>
         </Flex>
         <Flex alignItems={'end'}>
@@ -79,88 +136,93 @@ const Footer = () => {
               mt={'32px'}
               spacing={'0.75rem'}
             >
-              <Text fontWeight={600}>백엔드</Text>
-              <Text>김범우</Text>
-              <Divider
-                h={'0.75rem'}
-                orientation="vertical"
-                border={'1px'}
-                borderColor={'gray.300'}
-              />
-              <Text>주홍석</Text>
-              <Divider
-                h={'0.75rem'}
-                orientation="vertical"
-                border={'1px'}
-                borderColor={'gray.300'}
-              />
-              <Text>최지연</Text>
+              <Text
+                w={'3.25rem'}
+                fontWeight={600}
+              >
+                {FOOTER_CONTENT.team.backend}
+              </Text>
+              {FOOTER_CONTENT.team.backendMembers.map((member, idx) => (
+                <>
+                  <Text key={member}>{member}</Text>
+                  {idx !== FOOTER_CONTENT.team.backendMembers.length - 1 && (
+                    <Divider
+                      h={'0.75rem'}
+                      orientation="vertical"
+                      border={'1px'}
+                      borderColor={'gray.300'}
+                    />
+                  )}
+                </>
+              ))}
             </HStack>
             <HStack spacing={'0.75rem'}>
-              <Text fontWeight={600}>프론트엔드</Text>
-              <Text>신동호</Text>
-              <Divider
-                h={'0.75rem'}
-                orientation="vertical"
-                border={'1px'}
-                borderColor={'gray.300'}
-              />
-              <Text>우지호</Text>
-              <Divider
-                h={'0.75rem'}
-                orientation="vertical"
-                border={'1px'}
-                borderColor={'gray.300'}
-              />
-              <Text>이민희</Text>
+              <Text
+                w={'3.25rem'}
+                fontWeight={600}
+              >
+                {FOOTER_CONTENT.team.frontend}
+              </Text>
+              {FOOTER_CONTENT.team.frontendMembers.map((member, idx) => (
+                <>
+                  <Text key={member}>{member}</Text>
+                  {idx !== FOOTER_CONTENT.team.frontendMembers.length - 1 && (
+                    <Divider
+                      h={'0.75rem'}
+                      orientation="vertical"
+                      border={'1px'}
+                      borderColor={'gray.300'}
+                    />
+                  )}
+                </>
+              ))}
             </HStack>
-
             <HStack
               mt={'32px'}
               spacing={'0.75rem'}
             >
-              <Text fontWeight={600}>서울특별시 프롱구 백둥대로 4기</Text>
+              <Text fontWeight={600}>{FOOTER_CONTENT.team.address}</Text>
               <Divider
                 h={'0.75rem'}
                 orientation="vertical"
                 border={'1px'}
                 borderColor={'gray.300'}
               />
-              <Text>전화번호: 000-0000-0000</Text>
+              <Text>{FOOTER_CONTENT.team.phoneNumber}</Text>
             </HStack>
           </Flex>
           <HStack
             mt={'34px'}
             color={'gray.400'}
-            spacing={'8rem'}
+            spacing={'5rem'}
             alignItems={'start'}
           >
             <Flex direction={'column'}>
-              <Text fontWeight={600}>이력서</Text>
-              <Link //Todo: 이력서 관리 라우팅 후 연결
-                to={'/'}
-              >
-                <Text mt={'1.13rem'}>이력서 관리</Text>
+              <Text fontWeight={600}>{FOOTER_CONTENT.pages.resume.text}</Text>
+              <Link to={FOOTER_CONTENT.pages.resume.link.resumeManagement.url}>
+                <Text mt={'1.13rem'}>{FOOTER_CONTENT.pages.resume.link.resumeManagement.text}</Text>
               </Link>
             </Flex>
             <Flex direction={'column'}>
-              <Text fontWeight={600}>이벤트</Text>
-              <Link to={'/event/view'}>
-                <Text mt={'1.13rem'}>진행중인 이벤트</Text>
+              <Text fontWeight={600}>{FOOTER_CONTENT.pages.event.text}</Text>
+              <Link to={FOOTER_CONTENT.pages.event.link.ongoingEvent.url}>
+                <Text mt={'1.13rem'}>{FOOTER_CONTENT.pages.event.link.ongoingEvent.text}</Text>
               </Link>
             </Flex>
             <Flex direction={'column'}>
-              <Link to={'/event/view'}>
-                <Text fontWeight={600}>커뮤니티</Text>
+              <Link to={FOOTER_CONTENT.pages.community.link.mentorCoffeeChat.url}>
+                <Text fontWeight={600}>{FOOTER_CONTENT.pages.community.text}</Text>
               </Link>
-              <Link to={'/coffee-chat'}>
-                <Text mt={'1.13rem'}>멘토 커피챗</Text>
+              <Link to={FOOTER_CONTENT.pages.community.link.mentorCoffeeChat.url}>
+                <Text mt={'1.13rem'}>
+                  {FOOTER_CONTENT.pages.community.link.mentorCoffeeChat.text}
+                </Text>
               </Link>
-              <Link to={'/general'}>
-                <Text>자유게시판</Text>
+              <Link to={FOOTER_CONTENT.pages.community.link.freeBoard.url}>
+                <Text>{FOOTER_CONTENT.pages.community.link.freeBoard.text}</Text>
               </Link>
-              <Link to={'/resume/references'}>
-                <Text>합격 이력서</Text>
+              <Link to={FOOTER_CONTENT.pages.community.link.successfulResumes.url}>
+                <Text>{FOOTER_CONTENT.pages.community.link.successfulResumes.text}</Text>
               </Link>
             </Flex>
           </HStack>
@@ -176,7 +238,7 @@ const Footer = () => {
           fontSize={'0.75rem'}
           fontWeight={700}
         >
-          ⓒ 2023 이력, 써{' '}
+          {FOOTER_CONTENT.copyright}
         </Text>
       </Flex>
     </Box>
