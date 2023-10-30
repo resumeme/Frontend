fa5f3578669d0d8ef0bf78f98264e8eea7d4d2a4
Dongho Shin <kakkiid@gmail.com>
2023-10-30 15:54:58 +0900

    feat: 보더박스 커스텀 스토리 추가

--- src/components/atoms/BorderBox/index.stories.ts
+++ src/components/atoms/BorderBox/index.stories.ts
@@ -12,13 +12,27 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: '컨테이너 박스',
+    children: '기본 보더박스',
   },
 };
 
 export const HasShadow: Story = {
   args: {
-    children: '컨테이너 박스',
+    children: '그림자가 있는 보더박스',
     hasShadow: true,
+  },
+};
+
+export const Custom: Story = {
+  args: {
+    children: '커스텀 보더박스',
+    borderRadius: 'md',
+    width: '500px',
+    height: '60px',
+    px: '3',
+    py: '3',
+    bg: 'primary.300',
+    color: 'gray.800',
+    fontWeight: 'bold',
   },
 };
