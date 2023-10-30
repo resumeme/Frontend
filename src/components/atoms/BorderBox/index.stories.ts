9c89b15c228db527345f79896c00b1ecab58348f
Dongho Shin <kakkiid@gmail.com>
2023-10-30 16:17:30 +0900

    Gmmq 154 feat: BorderBox 공통 컴포넌트 개발 (#17)
    * feat: BorderBox 공통 컴포넌트
    
    * feat: 보더박스 커스텀 스토리 추가

---
+++ src/components/atoms/BorderBox/index.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BorderBox from './BorderBox';
+
+const meta = {
+  title: 'Resumeme/Components/BorderBox',
+  component: BorderBox,
+  tags: ['autodocs'],
+} satisfies Meta<typeof BorderBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: '기본 보더박스',
+  },
+};
+
+export const HasShadow: Story = {
+  args: {
+    children: '그림자가 있는 보더박스',
+    hasShadow: true,
+  },
+};
+
+export const Custom: Story = {
+  args: {
+    children: '커스텀 보더박스',
+    borderRadius: 'md',
+    width: '500px',
+    height: '60px',
+    px: '3',
+    py: '3',
+    bg: 'primary.300',
+    color: 'gray.800',
+    fontWeight: 'bold',
+  },
+};
