355282ab2056253419717fa0995bfc6436221531
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2023-10-30 15:09:28 +0900

    Gmmq 148 feat: 공통 button 컴포넌트 개발 (#13)
    * feat: 차크라 button theme 추가
    
    * feat: 버튼 컴포넌트 생성
    
    * feat: 버튼 스토리북 작성
    
    * refactor: 버튼 props 확장

---
+++ src/theme/components/button.ts
@@ -0,0 +1,47 @@
+import { defineStyleConfig } from '@chakra-ui/react';
+
+const Button = defineStyleConfig({
+  baseStyle: {
+    borderRadius: '0.625rem',
+  },
+  sizes: {
+    lg: {
+      w: '25.25rem',
+      h: '2.69rem',
+      fontSize: '1rem',
+    },
+    md: {
+      w: '10rem',
+      h: '2.69rem',
+      fontSize: '1rem',
+    },
+    sm: {
+      w: '5.125rem',
+      h: '2.69rem',
+      fontSize: '1rem',
+    },
+    xs: {
+      w: '3.16rem',
+      h: '2rem',
+      fontSize: '0.875rem',
+    },
+    full: {
+      w: '100%',
+      h: '2.69rem',
+    },
+  },
+  variants: {
+    cancel: {
+      border: '1px solid',
+      borderColor: 'gray.300',
+      color: 'gray.400',
+      bg: 'gray.100',
+    },
+    default: {
+      color: 'gray.100',
+      bg: 'primary.900',
+    },
+  },
+  defaultProps: {},
+});
+export { Button };
