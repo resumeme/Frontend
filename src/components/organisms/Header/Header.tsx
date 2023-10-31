c8dd4ad92d269d366965ea2179ddaf6eff306885
Dongho Shin <kakkiid@gmail.com>
2023-10-31 15:51:41 +0900

    chore: checkout

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -3,7 +3,14 @@ import { Container } from '@chakra-ui/react';
 const Header = () => {
   return (
     <>
-      <Container>헤더</Container>
+      <Container
+        className="header"
+        bg="white"
+        w="full"
+        h="65px"
+      >
+        헤더
+      </Container>
     </>
   );
 };
