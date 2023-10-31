7938d6c06848acf3bd025daa4ee63fc978479c9e
이민희 <mhl98@dgu.ac.kr>
2023-11-01 03:24:32 +0900

    refactor: 스토리지 업무경험 데이터 가져오기

--- src/components/organisms/ResumeCategoryCareer/ResumeCategoryCareer.tsx
+++ src/components/organisms/ResumeCategoryCareer/ResumeCategoryCareer.tsx
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { BorderBox } from '~/components/atoms/BorderBox';
 import { CategoryAddHeader } from '~/components/molecules/CategoryAddHeader';
 import CareerForm from '~/components/organisms/ResumeCategoryCareer/CareerForm';
+import { useStorage } from '~/hooks/useStorage';
 import Career from '~/types/career';
 import { Duty } from '~/types/career';
 import { DateString } from '~/types/dateString';
@@ -27,20 +28,23 @@ const careerDefaultItem: DefaultCareer = {
   careerStartDate: '',
 };
 
+const CAREER_ITEMS_KEY = 'career-items';
 const ResumeCategoryCareer = () => {
   /**TODO zustand persist로 폼 입력값 스토리지에 저장하기 (배열)
    * 저장한 값을 불러와서 map으로 돌린다
    * FIXME useState를 zustand store에서 불러오도록 수정하기
    */
-  const [careerItems, setCareerItems] = useState<object[]>([]);
-  const handleItemAdd = () => {
-    setCareerItems((prevItems) => [...prevItems, careerDefaultItem]);
-  };
+  // const [careerItems, setCareerItems] = useState<object[]>([]);
+  const { storageValue: careerItems, setValue } = useStorage<DefaultCareer[]>({
+    key: CAREER_ITEMS_KEY,
+    initialValue: [],
+  });
+
   return (
     <div style={{ width: '960px', minHeight: '100px' }}>
       <CategoryAddHeader
         categoryTitle="업무 경험"
-        onAddItem={handleItemAdd}
+        onAddItem={() => setValue([...careerItems, careerDefaultItem])}
       />
       <VStack
         marginTop={'1.56rem'}
