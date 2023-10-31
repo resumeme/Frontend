89f3d8efbb53967d4c5e685c69426b0cfbb35616
backward99 <wjeeho@naver.com>
2023-10-31 10:18:03 +0900

    feat: 이벤트 생성 페이지 컴포넌트 구성

--- src/pages/EventPage/CreateEventPage.tsx
+++ src/pages/EventPage/CreateEventPage.tsx
@@ -1,20 +1,17 @@
 import {
+  Box,
   Checkbox,
   CheckboxGroup,
   Flex,
   FormControl,
-  FormErrorMessage,
+  FormLabel,
   HStack,
-  Input,
   Text,
   Textarea,
-  VStack,
 } from '@chakra-ui/react';
 import { Controller, useForm } from 'react-hook-form';
-import { BorderBox } from '~/components/atoms/BorderBox';
 import { Button } from '~/components/atoms/Button';
-import FormLabel from '~/components/atoms/FormLabel/FormLabel';
-import FormDateInput from '~/components/molecules/FormDateInpur/FormDateInput';
+import { FormInput } from '~/components/molecules/FormInput';
 import { FormInputSchema } from '~/types/formInput';
 
 const CreateEventPage = () => {
@@ -108,136 +105,118 @@ const CreateEventPage = () => {
           direction={'column'}
         >
           {Object.keys(FORM_TEXT_INPUT_SCHEMA).map((key) => (
-            <BorderBox key={key}>
-              <FormControl isInvalid={!!errors[key]}>
-                <HStack spacing={0}>
-                  <FormLabel
-                    htmlFor={key}
-                    isRequired={'required' in FORM_TEXT_INPUT_SCHEMA[key].errorTypes}
-                  >
-                    {FORM_TEXT_INPUT_SCHEMA[key].label}
-                  </FormLabel>
-                  <VStack w={'100%'}>
-                    <Input
-                      id={key}
-                      placeholder={FORM_TEXT_INPUT_SCHEMA[key].placeholder}
-                      {...register(key, { ...FORM_TEXT_INPUT_SCHEMA[key].errorTypes })}
-                    />
-                    {errors[key]?.message && (
-                      <FormErrorMessage>{errors[key]?.message as string}</FormErrorMessage>
-                    )}
-                  </VStack>
-                </HStack>
-              </FormControl>
-            </BorderBox>
+            <FormInput
+              key={key}
+              isRequired={'required' in FORM_TEXT_INPUT_SCHEMA[key].errorTypes}
+              direction="row"
+              id={key}
+              placeholder={FORM_TEXT_INPUT_SCHEMA[key].placeholder}
+              label={FORM_TEXT_INPUT_SCHEMA[key].label}
+              register={{
+                ...register(key, { ...FORM_TEXT_INPUT_SCHEMA[key].errorTypes }),
+              }}
+              errors={errors}
+              type={FORM_TEXT_INPUT_SCHEMA[key].type}
+            />
           ))}
-          <BorderBox>
-            <FormControl isInvalid={!!errors['jobs']}>
-              <HStack>
-                <FormLabel isRequired>직무</FormLabel>
-                <Controller
-                  name="jobs"
-                  control={control}
-                  render={({ field }) => (
-                    <CheckboxGroup {...field}>
-                      <Checkbox value="web">웹 풀스택</Checkbox>
-                      <Checkbox value="frontend">프론트엔드</Checkbox>
-                      <Checkbox value="backend">백엔드</Checkbox>
-                      <Checkbox value="devops">데브옵스</Checkbox>
-                    </CheckboxGroup>
-                  )}
-                />
-              </HStack>
-            </FormControl>
-          </BorderBox>
-
-          <BorderBox>
-            <Flex>
-              {Object.keys(FORM_EVENT_DATE_INPUT_SCHEMA).map((key) => (
-                <FormControl
-                  isInvalid={!!errors[key]}
-                  key={key}
-                >
-                  <HStack spacing={0}>
-                    {FORM_EVENT_DATE_INPUT_SCHEMA[key].label && (
-                      <FormLabel
-                        htmlFor={key}
-                        isRequired={'required' in FORM_EVENT_DATE_INPUT_SCHEMA[key].errorTypes}
-                      >
-                        {FORM_EVENT_DATE_INPUT_SCHEMA[key].label}
-                      </FormLabel>
-                    )}
-                    <Flex
-                      direction={'column'}
-                      justifyContent={'space-between'}
-                    >
-                      <FormDateInput
-                        w={'16rem'}
-                        type="datetime-local"
-                        control={control}
-                        dateRegister={{
-                          ...register(key, { ...FORM_EVENT_DATE_INPUT_SCHEMA[key].errorTypes }),
-                        }}
-                        timeRegister={{
-                          ...register(`${key}Time`, {
-                            ...FORM_EVENT_DATE_INPUT_SCHEMA[key].errorTypes,
-                          }),
-                        }}
-                        id={key}
-                      />
-                    </Flex>
-                  </HStack>
-                </FormControl>
-              ))}
-            </Flex>
-          </BorderBox>
-          <BorderBox>
-            <FormControl
-              isInvalid={!!errors['endEventDate']}
-              key={'endEventDate'}
+          <FormControl>
+            <FormLabel
+              fontWeight={600}
+              lineHeight={'normal'}
+              fontSize={'1.125rem'}
+              w={'6.9375rem'}
+              minW={'6.9375rem'}
+              htmlFor={'eventContent'}
+              mx={0}
+              mt={'0.5rem'}
+              color={'gray.700'}
+              p={0}
             >
-              <HStack spacing={0}>
-                {FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label && (
-                  <FormLabel
-                    htmlFor={'endEventDate'}
-                    isRequired={
-                      'required' in FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes
-                    }
-                  >
-                    {FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label}
-                  </FormLabel>
-                )}
-                <Flex direction={'column'}>
-                  <FormDateInput
-                    w={'16rem'}
-                    control={control}
-                    dateRegister={{
-                      ...register('endEventDate', {
-                        ...FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes,
-                      }),
-                    }}
-                    id={'endEventDate'}
-                  />
-                </Flex>
-              </HStack>
-            </FormControl>
-          </BorderBox>
-          <BorderBox>
-            <FormControl>
-              <Flex>
-                <FormLabel
-                  htmlFor="eventContent"
-                  isRequired
+              직무
+              <Box
+                as="span"
+                color="primary.900"
+              >
+                {' '}
+                *
+              </Box>
+            </FormLabel>
+            <Controller
+              name="jobs"
+              control={control}
+              render={({ field }) => (
+                <CheckboxGroup {...field}>
+                  <Checkbox value="web">웹 풀스택</Checkbox>
+                  <Checkbox value="frontend">프론트엔드</Checkbox>
+                  <Checkbox value="backend">백엔드</Checkbox>
+                  <Checkbox value="devops">데브옵스</Checkbox>
+                </CheckboxGroup>
+              )}
+            />
+          </FormControl>
+          <Flex justifyContent={'space-between'}>
+            {Object.keys(FORM_EVENT_DATE_INPUT_SCHEMA).map((key) => (
+              <FormInput
+                key={key}
+                isRequired={'required' in FORM_EVENT_DATE_INPUT_SCHEMA[key].errorTypes}
+                direction="row"
+                id={key}
+                placeholder={FORM_EVENT_DATE_INPUT_SCHEMA[key].placeholder}
+                label={FORM_EVENT_DATE_INPUT_SCHEMA[key].label}
+                register={{
+                  ...register(key, { ...FORM_EVENT_DATE_INPUT_SCHEMA[key].errorTypes }),
+                }}
+                errors={errors}
+                type={FORM_EVENT_DATE_INPUT_SCHEMA[key].type}
+                control={control}
+              />
+            ))}
+          </Flex>
+          <FormInput
+            isRequired={'required' in FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes}
+            direction="row"
+            id={'endEventDate'}
+            placeholder={FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].placeholder}
+            label={FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label}
+            register={{
+              ...register('endEventDate', {
+                ...FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes,
+              }),
+            }}
+            errors={errors}
+            type={FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].type}
+            control={control}
+          />
+
+          <FormControl>
+            <Flex>
+              <FormLabel
+                fontWeight={600}
+                lineHeight={'normal'}
+                fontSize={'1.125rem'}
+                w={'6.9375rem'}
+                minW={'6.9375rem'}
+                htmlFor={'eventContent'}
+                mx={0}
+                mt={'0.5rem'}
+                color={'gray.700'}
+                p={0}
+              >
+                {FORM_TEXT_AREA_INPUT_SCHEMA['eventContent'].label}
+                <Box
+                  as="span"
+                  color="primary.900"
                 >
-                  {FORM_TEXT_AREA_INPUT_SCHEMA['eventContent'].label}
-                </FormLabel>
-                <Textarea
-                  id="eventContent"
-                  placeholder={FORM_TEXT_AREA_INPUT_SCHEMA['eventContent'].placeholder}
-                />
-              </Flex>
-            </FormControl>
-          </BorderBox>
+                  {' '}
+                  *
+                </Box>
+              </FormLabel>
+              <Textarea
+                id="eventContent"
+                placeholder={FORM_TEXT_AREA_INPUT_SCHEMA['eventContent'].placeholder}
+              />
+            </Flex>
+          </FormControl>
         </Flex>
         <Flex>
           <Button
