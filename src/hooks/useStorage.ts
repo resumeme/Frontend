cc0acac67c06c8db13f9484688ba87fdf068fb2f
이민희 <mhl98@dgu.ac.kr>
2023-11-01 02:58:43 +0900

    fix: useStorage 훅 수정 - 초기 저장 시 스토리지에 initialValue 저장

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -2,21 +2,31 @@ import { useState } from 'react';
 
 type UseLocalStorage<T> = {
   key: string;
-  initialData: T;
-  storageType: 'local' | 'session';
+  initialValue: T;
+  storageType?: 'local' | 'session';
 };
 
-export const useStorage = <T>({ key, initialData, storageType = 'local' }: UseLocalStorage<T>) => {
+export const useStorage = <T>({ key, initialValue, storageType = 'local' }: UseLocalStorage<T>) => {
   const storage = storageType === 'local' ? localStorage : sessionStorage;
 
   const [storageValue, setStorageValue] = useState<T>(() => {
-    const storedData = storage.getItem(key);
-    return storedData ? JSON.parse(storedData) : initialData;
+    try {
+      const value = storage.getItem(key);
+      if (!value) {
+        storage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+      }
+      return JSON.parse(value);
+    } catch (e) {
+      console.error(e);
+      return initialValue;
+    }
   });
 
   const setValue = (value: T) => {
     try {
-      setStorageValue((prevValue) => ({ ...prevValue, value }));
+      setStorageValue(value);
+      storage.setItem(key, JSON.stringify(value));
     } catch (e) {
       console.error(e);
     }
