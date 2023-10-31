8dd2abd479b515fec6ad4db505b6178477252ff3
이민희 <mhl98@dgu.ac.kr>
2023-10-31 15:46:56 +0900

    feat: 회사명, 직무 form 추가

--- src/components/organisms/ResumeCategoryCareer/ResumeCategoryCareer.tsx
+++ src/components/organisms/ResumeCategoryCareer/ResumeCategoryCareer.tsx
@@ -1,3 +1,9 @@
+import { FormControl, FormLabel, VStack, Input, FormErrorMessage } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { v4 as uuidv4 } from 'uuid';
+import { BorderBox } from '~/components/atoms/BorderBox';
+import { Button } from '~/components/atoms/Button';
 import { CategoryAddHeader } from '~/components/molecules/CategoryAddHeader';
 import Career from '~/types/career';
 import { Duty } from '~/types/career';
@@ -15,9 +21,9 @@ interface DefaultCareer extends Omit<Career, 'position' | 'duties' | 'careerStar
 }
 
 const ResumeCategoryCareer = () => {
-  const careerItems: DefaultCareer[] = [];
+  const [careerItems, setCareerItems] = useState<DefaultCareer[]>([]);
   const handleItemAdd = () => {
-    careerItems.push(careerDefaultItem);
+    setCareerItems((prevItems) => [...prevItems, careerDefaultItem]);
   };
   return (
     <div style={{ width: '960px', minHeight: '100px' }}>
@@ -25,6 +31,19 @@ const ResumeCategoryCareer = () => {
         categoryTitle="업무 경험"
         onAddItem={handleItemAdd}
       />
+      <VStack
+        marginTop={'1.56rem'}
+        spacing={'1rem'}
+      >
+        {careerItems.map(() => (
+          <BorderBox
+            key={uuidv4()}
+            w={'100%'}
+          >
+            <CareerForm />
+          </BorderBox>
+        ))}
+      </VStack>
     </div>
   );
 };
@@ -35,6 +54,49 @@ const careerDefaultItem: DefaultCareer = {
   isCurrentlyEmployed: false,
   careerContent: '',
   careerStartDate: '',
+};
+
+const CareerForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = handleSubmit((values) => {
+    console.log('values', values);
+  });
+  // console.log('errors', errors);
+
+  return (
+    <form onSubmit={onSubmit}>
+      {/*FIXME 각각의 Input, Label 컴포넌트 대체하기 */}
+      <FormControl isInvalid={Boolean(errors.name)}>
+        <FormLabel htmlFor="companyName">회사명</FormLabel>
+        <Input
+          id="companyName"
+          {...register('companyName', { required: '회사명을 입력하세요' })}
+        />
+        <FormErrorMessage>
+          {errors.companyName && errors.companyName.message?.toString()}
+        </FormErrorMessage>
+        <FormLabel htmlFor="position">직무</FormLabel>
+        <Input
+          id="position"
+          {...register('position', { required: '직무를 입력하세요.' })}
+        />
+        <FormErrorMessage>
+          {errors.position && errors.position.message?.toString()}
+        </FormErrorMessage>
+      </FormControl>
+      <Button
+        size={'sm'}
+        type="submit"
+      >
+        저장
+      </Button>
+    </form>
+  );
 };
 
 export default ResumeCategoryCareer;
--- src/components/organisms/ResumeCategoryCareer/index.stories.tsx
+++ src/components/organisms/ResumeCategoryCareer/index.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import ResumeCategoryCareer from './ResumeCategoryCareer';
 
 const meta = {
@@ -12,8 +12,7 @@ const meta = {
 } satisfies Meta<typeof ResumeCategoryCareer>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
+export const Default = () => {
+  return <ResumeCategoryCareer />;
 };
