8b54f40a2e2d7f25d60b4c6d33fd86a01744fa0d
backward99 <wjeeho@naver.com>
2023-10-31 15:27:45 +0900

    feat: FormDateInput 스토리 파일 생성

---
+++ src/components/molecules/FormDateInpur/index.stories.tsx
@@ -0,0 +1,177 @@
+import { Flex, FormControl, HStack } from '@chakra-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+import FormDateInput from './FormDateInput';
+import { BorderBox } from '~/components/atoms/BorderBox';
+import { Button } from '~/components/atoms/Button';
+import FormLabel from '~/components/atoms/FormLabel/FormLabel';
+import { FormInputSchema } from '~/types/formInput';
+
+const meta = {
+  title: 'Resumeme/Components/FormDateInput',
+  component: FormDateInput,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['default', 'cancel'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['lg', 'md', 'sm', 'xs', 'full'],
+      control: { type: 'radio' },
+    },
+    children: {
+      control: 'text',
+    },
+  },
+} satisfies Meta<typeof FormDateInput>;
+
+export default meta;
+
+export const DefaultFormDateInput = () => {
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (values: { [key: string]: string }) => {
+    return new Promise(() => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+      }, 3000);
+    });
+  };
+
+  const FORM_RESUME_DATE_INPUT_SCHEMA: FormInputSchema = {
+    endEventDate: {
+      type: 'date',
+      label: '첨삭 종료일',
+      placeholder: '',
+      errorTypes: {
+        required: true,
+      },
+    },
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <BorderBox>
+        <FormControl
+          isInvalid={!!errors['endEventDate']}
+          key={'endEventDate'}
+        >
+          <HStack spacing={0}>
+            {FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label && (
+              <FormLabel
+                htmlFor={'endEventDate'}
+                isRequired={'required' in FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes}
+              >
+                {FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label}
+              </FormLabel>
+            )}
+            <Flex direction={'column'}>
+              <FormDateInput
+                w={'16rem'}
+                control={control}
+                dateRegister={{
+                  ...register('endEventDate', {
+                    ...FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes,
+                  }),
+                }}
+                id={'endEventDate'}
+              />
+            </Flex>
+          </HStack>
+        </FormControl>
+      </BorderBox>
+      <Button
+        size={'md'}
+        ml={'2.06rem'}
+        mt={'2.62rem'}
+        isLoading={isSubmitting}
+        type="submit"
+      >
+        등록하기
+      </Button>
+    </form>
+  );
+};
+
+export const TimeFormDateInput = () => {
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (values: { [key: string]: string }) => {
+    return new Promise(() => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+      }, 3000);
+    });
+  };
+
+  const FORM_RESUME_DATE_INPUT_SCHEMA: FormInputSchema = {
+    endEventDate: {
+      type: 'datetime-local',
+      label: '첨삭 종료일',
+      placeholder: '',
+      errorTypes: {
+        required: true,
+      },
+    },
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <BorderBox>
+        <FormControl
+          isInvalid={!!errors['endEventDate']}
+          key={'endEventDate'}
+        >
+          <HStack spacing={0}>
+            {FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label && (
+              <FormLabel
+                htmlFor={'endEventDate'}
+                isRequired={'required' in FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes}
+              >
+                {FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].label}
+              </FormLabel>
+            )}
+            <Flex direction={'column'}>
+              <FormDateInput
+                type="datetime-local"
+                w={'16rem'}
+                control={control}
+                dateRegister={{
+                  ...register('endEventDate', {
+                    ...FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes,
+                  }),
+                }}
+                timeRegister={{
+                  ...register('endEventDateTime', {
+                    ...FORM_RESUME_DATE_INPUT_SCHEMA['endEventDate'].errorTypes,
+                  }),
+                }}
+                id={'endEventDate'}
+              />
+            </Flex>
+          </HStack>
+        </FormControl>
+      </BorderBox>
+      <Button
+        size={'md'}
+        ml={'2.06rem'}
+        mt={'2.62rem'}
+        isLoading={isSubmitting}
+        type="submit"
+      >
+        등록하기
+      </Button>
+    </form>
+  );
+};
---
+++ src/components/molecules/FormDateInpur/index.ts
@@ -0,0 +1,3 @@
+import FormDateInput from './FormDateInput';
+
+export { FormDateInput };
