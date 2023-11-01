bb2edd02e8b1939e39f98985e7d62a471846f6cc
backward99 <wjeeho@naver.com>
2023-11-01 23:35:41 +0900

    fix: 컴포넌트 별 스토리 파일 수정

--- src/components/molecules/FormDateInput/index.stories.tsx
+++ src/components/molecules/FormDateInput/index.stories.tsx
@@ -1,5 +1,10 @@
 import type { Meta } from '@storybook/react';
+import { useForm } from 'react-hook-form';
 import FormDateInput from './FormDateInput';
+import { FormControl } from '../FormControl';
+import { BorderBox } from '~/components/atoms/BorderBox';
+import { Button } from '~/components/atoms/Button';
+import { FormLabel } from '~/components/atoms/FormLabel';
 
 const meta = {
   title: 'Resumeme/Components/FormDateInput',
@@ -10,20 +15,41 @@ const meta = {
 export default meta;
 
 export const DefaultFormDateInput = () => {
-  // const {
-  //   control,
-  //   handleSubmit,
-  //   register,
-  //   formState: { errors, isSubmitting },
-  // } = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
-  // const onSubmit = (values: { [key: string]: string }) => {
-  //   return new Promise(() => {
-  //     setTimeout(() => {
-  //       alert(JSON.stringify(values, null, 2));
-  //     }, 3000);
-  //   });
-  // };
+  const onSubmit = (values: { [key: string]: string }) => {
+    return new Promise(() => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+      }, 3000);
+    });
+  };
 
-  return <></>;
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <BorderBox>
+        <FormControl isInvalid={!!errors['endDate']}>
+          <FormLabel isRequired={true}>첨삭 종료일</FormLabel>
+          <FormDateInput
+            w={'100%'}
+            maxW={'386px'}
+            register={{ ...register('endDate', { required: true }) }}
+          />
+        </FormControl>
+      </BorderBox>
+      <Button
+        size={'md'}
+        ml={'2.06rem'}
+        mt={'2.62rem'}
+        isLoading={isSubmitting}
+        type="submit"
+      >
+        등록하기
+      </Button>
+    </form>
+  );
 };
