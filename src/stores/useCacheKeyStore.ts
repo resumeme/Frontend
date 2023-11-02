ba0810663af0c724a9709564fee3d312f4113239
이민희 <mhl98@dgu.ac.kr>
2023-11-02 16:17:16 +0900

    chore: 파일 이름 변경

--- src/stores/useCacheKeyStore.ts
+++ src/stores/useCacheKeyStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import constants from '~/constants';
 
-export const useSignUpCacheKey = create(
+export const useCacheKeyStore = create(
   persist(
     (set) => ({
       cacheKey: '',
