dc45838a78067f850cd3a762ee77f2fbb581dec6
Dongho Shin <kakkiid@gmail.com>
2023-11-04 03:06:24 +0900

    feat: TitleInput 컴포넌트 formcontorl 프로퍼티 추가

--- src/components/atoms/TitleInput/TitleInput.tsx
+++ src/components/atoms/TitleInput/TitleInput.tsx
@@ -1,25 +1,42 @@
-import { Input, InputProps } from '@chakra-ui/react';
+import { Input, InputProps, FormErrorMessage, Flex } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import { FieldError, FieldErrors, UseFormRegisterReturn } from 'react-hook-form';
 
-type TitleInputProps = InputProps;
+type TitleInputProps = InputProps & {
+  id: string;
+  register: UseFormRegisterReturn;
+  error?: FieldError | FieldErrors;
+  children?: ReactNode;
+} & Omit<InputProps, 'type'>;
 
-const TitleInput = ({ children, ...props }: TitleInputProps) => {
+const TitleInput = ({ id, register, error, children, ...props }: TitleInputProps) => {
+  const errorMessage = error && 'message' in error ? (error.message as string) : '';
+
   return (
-    <Input
-      width={'100%'}
-      height={'60px'}
-      border={'none'}
-      color={'gray.800'}
-      borderBottom={'1px'}
-      borderRadius={'none'}
-      borderColor={'gray.300'}
-      placeholder="이력서 제목"
-      _placeholder={{ color: 'gray.400', fontWeight: 'light' }}
-      fontSize={'32px'}
-      px={2}
-      {...props}
+    <Flex
+      direction={'column'}
+      flexGrow={1}
     >
-      {children}
-    </Input>
+      <Input
+        id={id}
+        width={'100%'}
+        height={'60px'}
+        border={'none'}
+        color={'gray.800'}
+        borderBottom={'1px'}
+        borderRadius={'none'}
+        borderColor={'gray.300'}
+        placeholder="이력서 제목"
+        _placeholder={{ color: 'gray.400', fontWeight: 'semibold' }}
+        fontSize={'32px'}
+        px={2}
+        {...register}
+        {...props}
+      >
+        {children}
+      </Input>
+      {error && <FormErrorMessage>{errorMessage}</FormErrorMessage>}
+    </Flex>
   );
 };
 
