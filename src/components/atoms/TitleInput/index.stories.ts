5c34373e4f81b1e177605ff965485db081063220
Dongho Shin <kakkiid@gmail.com>
2023-11-04 05:05:55 +0900

    fix: TitleInput 공통 컴포넌트 Storybook 수정

--- src/components/atoms/TitleInput/index.stories.ts
+++ src/components/atoms/TitleInput/index.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import TitleInput from './TitleInput';
 
 const meta = {
@@ -11,6 +11,5 @@ const meta = {
 } satisfies Meta<typeof TitleInput>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default = {};
