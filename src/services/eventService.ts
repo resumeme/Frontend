54ed9b7d12b2b31de4b59db029565903536b389d
backward99 <wjeeho@naver.com>
2023-11-04 10:06:43 +0900

    chore: 이벤트 생성 요청

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -19,6 +19,7 @@ export type CreateEvent = {
 const createEvent = async (data: CreateEvent) => {
   data.time.now = new Date().toISOString().substring(0, 16);
   data.time.endDate = new Date(data.time.endDate).toISOString().substring(0, 16);
+  // return await resumeMeAxios.post('/v1/events', { ...data });
 };
 
 export const useCreateEvent = () => {
