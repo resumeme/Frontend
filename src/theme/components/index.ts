de6672ef0b5c5f05c9c8832773f9a0e7fc21c208
backward99 <wjeeho@naver.com>
2023-11-04 15:55:14 +0900

    style: textarea theme 설정

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,5 +1,6 @@
 import { Avatar } from './avatar';
 import { Button } from './button';
 import { Input } from './input';
+import { Textarea } from './textarea';
 
-export { Input, Button, Avatar };
+export { Input, Button, Avatar, Textarea };
