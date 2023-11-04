656eb4feda9753c83c99ce767609d6a3156115e8
이민희 <mhl98@dgu.ac.kr>
2023-11-04 19:24:06 +0900

    fix: 이력서 id 타입 string으로 수정

--- src/api/resume/create/postResumeCareer.ts
+++ src/api/resume/create/postResumeCareer.ts
@@ -3,9 +3,9 @@ import { resumeMeAxios } from '~/api/axios';
 import Career from '~/types/career';
 import { ResumeMeErrorResponse } from '~/types/errorResponse';
 
-export const postResumeCareer = async (resumeId: number, resumeCareer: Career) => {
+export const postResumeCareer = async (resumeId: string, resumeCareer: Career) => {
   try {
-    const { data } = await resumeMeAxios.post(`/resume.../${resumeId}`, resumeCareer);
+    const { data } = await resumeMeAxios.post(`/api/v1/resume/${resumeId}/careers`, resumeCareer);
     return data;
   } catch (e) {
     if (isAxiosError<ResumeMeErrorResponse>(e)) {
