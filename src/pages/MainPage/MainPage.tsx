785994765f341c2cff5a6429be6b0022f75ef56e
이민희 <mhl98@dgu.ac.kr>
2023-11-05 17:12:03 +0900

    feat: 이력서 작성 버튼 생성, 작성 페이지로 라우팅

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,22 @@
+import { Button } from '~/components/atoms/Button';
+import { usePostCreateResume } from '~/queries/resume/create/usePostCreateResume';
+
 const MainPage = () => {
-  return <></>;
+  const resumeCreateMutation = usePostCreateResume();
+  const handleResumeCreate = () => {
+    resumeCreateMutation.mutate();
+  };
+  return (
+    <>
+      {/**FIXME - 아래는 임시 이력서 작성 버튼, 추후 대체할 것 */}
+      <Button
+        size={'md'}
+        onClick={handleResumeCreate}
+      >
+        새 이력서 작성
+      </Button>
+    </>
+  );
 };
 
 export default MainPage;
--- src/queries/resume/create/usePostCreateResume.ts
+++ src/queries/resume/create/usePostCreateResume.ts
@@ -1,9 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import { postCreateResume } from '~/api/resume/create/postCreateResume';
 
 export const usePostCreateResume = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationKey: ['postResume'],
     mutationFn: postCreateResume,
+    onSuccess: ({ id }) => {
+      navigate(`/resume/${id}/edit`);
+    },
   });
 };
