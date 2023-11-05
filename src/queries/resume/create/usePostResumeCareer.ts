ead81ef28949d54f688981cb03a5c6d86a40f69a
이민희 <mhl98@dgu.ac.kr>
2023-11-06 02:44:31 +0900

    refactor: 업무경험 작성 낙관적 업데이트 useMutation 설정

--- src/queries/resume/create/usePostResumeCareer.ts
+++ src/queries/resume/create/usePostResumeCareer.ts
@@ -1,9 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 import { postResumeCareer } from '~/api/resume/create/postResumeCareer';
+import Career from '~/types/career';
 
 export const usePostResumeCareer = () => {
+  const queryClient = useQueryClient();
+  const TARGET_QUERY_KEY = 'getResumeCareer';
   return useMutation({
     mutationKey: ['postCareer'],
     mutationFn: postResumeCareer,
+    onMutate: async (newCareer) => {
+      await queryClient.cancelQueries({ queryKey: [TARGET_QUERY_KEY] });
+      const previousCareers = queryClient.getQueryData([TARGET_QUERY_KEY]);
+      queryClient.setQueryData([TARGET_QUERY_KEY], (old: Career[]) => [...old, newCareer]);
+      return { previousCareers };
+    },
+    onError: (err, newCareer, context) => {
+      queryClient.setQueryData([TARGET_QUERY_KEY], context?.previousCareers);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [TARGET_QUERY_KEY] });
+    },
   });
 };
