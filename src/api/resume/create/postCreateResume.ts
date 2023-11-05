4d6cdaace32f0e0cb58614afcb0ef12150621e7d
이민희 <mhl98@dgu.ac.kr>
2023-11-05 17:14:15 +0900

    fix: 이력서 작성 헤더에 토큰 추가

--- src/api/resume/create/postCreateResume.ts
+++ src/api/resume/create/postCreateResume.ts
@@ -5,9 +5,17 @@ import { ResumeMeErrorResponse } from '~/types/errorResponse';
 
 export const postCreateResume = async () => {
   try {
-    const { data } = await resumeMeAxios.post(`/v1/resumes`, {
-      title: CONSTANTS.RESUME_DEFAULT_TITLE,
-    });
+    const { data } = await resumeMeAxios.post(
+      `/v1/resumes`,
+      {
+        title: CONSTANTS.RESUME_DEFAULT_TITLE,
+      },
+      {
+        headers: {
+          access: import.meta.env.VITE_TEMP_MENTEE_TOKEN,
+        },
+      },
+    );
     return data;
   } catch (e) {
     if (isAxiosError<ResumeMeErrorResponse>(e)) {
