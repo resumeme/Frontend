7a389e6c5b81bd8cb169c8c67261b4203a3b8d22
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2023-11-06 08:43:48 +0900

    Gmmq 268 feat: 이력서 외국어 (어학성적) 입력 폼 UI, react hook form (#46)
    * feat: 이력서 어학 타입 작성
    
    * feat: 이력서 어학성적 작성 폼
    
    * chore: todo 주석 추가

---
+++ src/components/organisms/ResumeCategoryLanguage/index.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import LanguageForm from './LanguageForm';
+
+const meta = {
+  title: 'Resumeme/Components/LanguageForm',
+  component: LanguageForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [
+    (Story) => {
+      const queryClient = new QueryClient();
+      return (
+        <QueryClientProvider client={queryClient}>
+          <Story />
+        </QueryClientProvider>
+      );
+    },
+  ],
+} satisfies Meta<typeof LanguageForm>;
+
+export default meta;
+
+export const Default = () => {
+  return <LanguageForm />;
+};
