40ced01f7b2d2961714da04127d77508bd1e63ee
backward99 <wjeeho@naver.com>
2023-11-06 00:57:37 +0900

    feat: Activity api 타입 정의

---
+++ src/types/activity.ts
@@ -0,0 +1,10 @@
+type Activity = {
+  activityName: string;
+  startDate: string;
+  endDate: string;
+  inProgress: boolean;
+  link: string;
+  description: string;
+};
+
+export type { Activity };
