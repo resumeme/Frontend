0fda70e4b7038f1d3bf3cbccbef2c040d677cfd5
backward99 <wjeeho@naver.com>
2023-11-06 13:27:15 +0900

    chore: 스토리 파일 queryclient 추가

--- src/components/organisms/ResumeCategoryTraining/index.stories.tsx
+++ src/components/organisms/ResumeCategoryTraining/index.stories.tsx
@@ -1,10 +1,22 @@
 import type { Meta } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TrainingForm from './TrainingForm';
 
 const meta = {
   title: 'Resumeme/Components/TrainingForm',
   component: TrainingForm,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      const queryClient = new QueryClient();
+
+      return (
+        <QueryClientProvider client={queryClient}>
+          <Story />
+        </QueryClientProvider>
+      );
+    },
+  ],
 } satisfies Meta<typeof TrainingForm>;
 
 export default meta;
