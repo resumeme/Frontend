7eaf40e35be6c70a6e79934858a355781f7202ee
이민희 <mhl98@dgu.ac.kr>
2023-11-06 12:25:31 +0900

    chore: 주석 제거

--- src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
+++ src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
@@ -5,9 +5,6 @@ import CareerDetail from '~/components/organisms/ResumeDetails/CareerDetails';
 import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
 
 const EditResumeTemplate = () => {
-  /**TODO - api 서버 오류 해결 후 주석 풀 것
-   * ResumeCategory의 detailsComponent에 상세 컴포넌트 props 전달하기
-   */
   const { id: resumeId } = useParams() as { id: string };
   const { data: careersData } = useGetResumeCareer({ resumeId });
   return (
