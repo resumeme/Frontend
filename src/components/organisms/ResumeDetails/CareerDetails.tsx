7d450593e0f19f7b4c1111fd2448dc78d5c5b07e
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2023-11-06 12:58:22 +0900

    Gmmq 300 feat: 업무 경험 작성 후 낙관적 업데이트, ResumeCategory 작성 (#48)
    * feat: 이력서 카테고리 공용 컴포넌트 작성
    
    * feat: 업무경험 상세 props로 ResumeCategory 전달
    
    * feat: 업무경험 상세 BorderBox 감쌈
    
    * refactor: 업무경험 작성 낙관적 업데이트 useMutation 설정
    
    * fix: usePostResumeCareer 안쓰는 변수 타입에러 disable
    
    * feat: 업무경험 낙관적 업데이트 반영, 업무 상세 데이터 props로 전달
    
    * chore: 주석 제거
    
    * style: 보더 박스 업무경험 아이템마다 작성하는 것으로 수정

--- src/components/organisms/ResumeDetails/CareerDetails.tsx
+++ src/components/organisms/ResumeDetails/CareerDetails.tsx
@@ -1,29 +1,25 @@
-// import { useParams } from 'react-router-dom';
-// import { v4 as uuidv4 } from 'uuid';
-// import { BorderBox } from '~/components/atoms/BorderBox';
-// import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
-// import Career from '~/types/career';
+import { v4 as uuidv4 } from 'uuid';
+import { BorderBox } from '~/components/atoms/BorderBox';
+import Career from '~/types/career';
 
-const CareerDetail = () => {
-  /**TODO - api 서버 오류 해결 후 주석 풀 것 */
-  // const { id: resumeId } = useParams() as { id: string };
-  // const { data } = useGetResumeCareer({ resumeId });
-  // if (!data) {
-  //   return;
-  // }
-  // return (
-  //   <>
-  //     {data?.map((company: Career) => {
-  //       return (
-  //         <BorderBox key={uuidv4()}>
-  //           <div>{company.companyName}</div>
-  //         </BorderBox>
-  //       );
-  //     })}
-  //   </>
-  // );
-  /**TODO - api 서버 오류 해결 후 삭제할 것 */
-  return <></>;
+const CareerDetails = ({ data }: { data: Career[] }) => {
+  if (!data) {
+    return;
+  }
+  return (
+    <>
+      {data?.map((company: Career) => {
+        return (
+          <BorderBox
+            key={uuidv4()}
+            w={'100%'}
+          >
+            <div>{company.companyName}</div>
+          </BorderBox>
+        );
+      })}
+    </>
+  );
 };
 
-export default CareerDetail;
+export default CareerDetails;
--- src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
+++ src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
@@ -1,13 +1,17 @@
+import { useParams } from 'react-router-dom';
 import CareerForm from '~/components/organisms/ResumeCategoryCareer/CareerForm';
 import ResumeCategory from '~/components/organisms/ResumeCategoryCareer/ResumeCategory';
 import CareerDetail from '~/components/organisms/ResumeDetails/CareerDetails';
+import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
 
 const EditResumeTemplate = () => {
+  const { id: resumeId } = useParams() as { id: string };
+  const { data: careersData } = useGetResumeCareer({ resumeId });
   return (
     <>
       <ResumeCategory
         categoryType="업무경험"
-        detailsComponent={<CareerDetail />}
+        detailsComponent={<CareerDetail data={careersData} />}
       >
         <CareerForm />
       </ResumeCategory>
