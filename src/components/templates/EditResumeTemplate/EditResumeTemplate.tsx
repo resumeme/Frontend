dcea4e64a91fdc87c905a6bfa4692441f804fbbe
이민희 <mhl98@dgu.ac.kr>
2023-11-06 12:48:57 +0900

    feat: 교육 상세 수정페이지에 붙임

--- src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
+++ src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
@@ -1,19 +1,28 @@
 import { useParams } from 'react-router-dom';
 import CareerForm from '~/components/organisms/ResumeCategoryCareer/CareerForm';
 import ResumeCategory from '~/components/organisms/ResumeCategoryCareer/ResumeCategory';
-import CareerDetail from '~/components/organisms/ResumeDetails/CareerDetails';
+import { TrainingForm } from '~/components/organisms/ResumeCategoryTraining';
+import { CareerDetails, TraningDetails } from '~/components/organisms/ResumeDetails';
 import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
+import { useGetResumeTraining } from '~/queries/resume/details/useGetResumeTraining';
 
 const EditResumeTemplate = () => {
   const { id: resumeId } = useParams() as { id: string };
   const { data: careersData } = useGetResumeCareer({ resumeId });
+  const { data: trainingsData } = useGetResumeTraining({ resumeId });
   return (
     <>
       <ResumeCategory
         categoryType="업무경험"
-        detailsComponent={<CareerDetail data={careersData} />}
+        detailsComponent={<CareerDetails data={careersData} />}
       >
         <CareerForm />
+      </ResumeCategory>
+      <ResumeCategory
+        categoryType="교육"
+        detailsComponent={<TraningDetails data={trainingsData} />}
+      >
+        <TrainingForm />
       </ResumeCategory>
     </>
   );
