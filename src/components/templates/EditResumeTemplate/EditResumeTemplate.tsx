ade05da965c49a1d37124881e99ce443f04a7598
이민희 <mhl98@dgu.ac.kr>
2023-11-06 13:32:46 +0900

    feat: 외국어 상세 컴포넌트 작성, 이력서 작성 템플릿에 추가

--- src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
+++ src/components/templates/EditResumeTemplate/EditResumeTemplate.tsx
@@ -2,15 +2,22 @@ import { useParams } from 'react-router-dom';
 import AwardForm from '~/components/organisms/ResumeCategoryAwards/AwardForm';
 import CareerForm from '~/components/organisms/ResumeCategoryCareer/CareerForm';
 import ResumeCategory from '~/components/organisms/ResumeCategoryCareer/ResumeCategory';
+import { LanguageForm } from '~/components/organisms/ResumeCategoryLanguage';
 import { TrainingForm } from '~/components/organisms/ResumeCategoryTraining';
-import { CareerDetails, TraningDetails } from '~/components/organisms/ResumeDetails';
+import {
+  CareerDetails,
+  TraningDetails,
+  LanguageDetails,
+} from '~/components/organisms/ResumeDetails';
 import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
+import { useGetResumeLanguage } from '~/queries/resume/details/useGetResumeLanguage';
 import { useGetResumeTraining } from '~/queries/resume/details/useGetResumeTraining';
 
 const EditResumeTemplate = () => {
   const { id: resumeId } = useParams() as { id: string };
   const { data: careersData } = useGetResumeCareer({ resumeId });
   const { data: trainingsData } = useGetResumeTraining({ resumeId });
+  const { data: languageData } = useGetResumeLanguage({ resumeId });
   return (
     <>
       <ResumeCategory
@@ -30,6 +37,12 @@ const EditResumeTemplate = () => {
         detailsComponent={<></>}
       >
         <AwardForm />
+      </ResumeCategory>
+      <ResumeCategory
+        categoryType="외국어"
+        detailsComponent={<LanguageDetails data={languageData} />}
+      >
+        <LanguageForm />
       </ResumeCategory>
     </>
   );
