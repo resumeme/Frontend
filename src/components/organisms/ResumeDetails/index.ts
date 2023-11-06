db7cb7328862ad9e8d95096be655083234f10e01
이민희 <mhl98@dgu.ac.kr>
2023-11-06 13:32:59 +0900

    chor: 인덱스 추가

--- src/components/organisms/ResumeDetails/index.ts
+++ src/components/organisms/ResumeDetails/index.ts
@@ -1,4 +1,5 @@
 import CareerDetails from './CareerDetails';
+import LanguageDetails from './LanguageDetails';
 import TraningDetails from './TrainingDetails';
 
-export { CareerDetails, TraningDetails };
+export { CareerDetails, TraningDetails, LanguageDetails };
