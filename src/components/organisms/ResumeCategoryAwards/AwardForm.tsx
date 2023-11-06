5132db6bfaa561037b45ac2eb54f5a511a1d80ef
backward99 <wjeeho@naver.com>
2023-11-06 12:51:29 +0900

    feat: 수상 및 경력 블럭 등록 api 연결

---
+++ src/components/organisms/ResumeCategoryAwards/AwardForm.tsx
@@ -0,0 +1,156 @@
+import { HStack, Flex } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BorderBox } from '~/components/atoms/BorderBox';
+import { Button } from '~/components/atoms/Button';
+import { FormLabel } from '~/components/atoms/FormLabel';
+import { FormControl } from '~/components/molecules/FormControl';
+import { FormDateInput } from '~/components/molecules/FormDateInput';
+import { FormTextarea } from '~/components/molecules/FormTextarea';
+import { FormTextInput } from '~/components/molecules/FormTextInput';
+import { usePostResumeAward } from '~/queries/resume/create/usePostResumeAward';
+import { Award } from '~/types/award';
+
+const AwardForm = () => {
+  const { id: resumeId } = useParams();
+  const { mutate: postResumeAward } = usePostResumeAward();
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Award>({
+    //Todo: useQuery 관련 작업 예상
+    // defaultValues: {
+    //   certificationTitle: '인증서',
+    //   acquisitionDate: '2023-10-01',
+    //   issuingAuthority: '발급기관',
+    //   link: 'https://resumeme.kro.kr',
+    //   description: '설명',
+    // },
+  });
+
+  const onSubmit: SubmitHandler<Award> = (resumeAward) => {
+    if (!resumeId) {
+      /**TODO - 토스트 대체! */
+      alert('존재하지 않는 이력서입니다.');
+      navigate(-1);
+      return;
+    }
+    postResumeAward({ resumeId, resumeAward });
+  };
+
+  return (
+    <BorderBox>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex
+          direction={'column'}
+          gap={'1.25rem'}
+        >
+          <FormControl isInvalid={Boolean(errors.certificationTitle)}>
+            <FormLabel
+              htmlFor="certificationTitle"
+              w={'8.625rem'}
+              isRequired
+            >
+              수상/취득 내용
+            </FormLabel>
+            <FormTextInput
+              placeholder="수상 및 자격증 정보를 입력해주세요. 예) 정보처리기사"
+              id="certificationTitle"
+              register={{
+                ...register('certificationTitle', { required: '필수 입력값입니다.' }),
+              }}
+              error={errors.certificationTitle}
+            />
+          </FormControl>
+          <Flex
+            w={'full'}
+            gap={'3rem'}
+          >
+            <FormControl
+              w={'60%'}
+              isInvalid={Boolean(errors.acquisitionDate)}
+            >
+              <FormLabel w={'8.625rem'}>취득 년월</FormLabel>
+              <FormDateInput
+                register={{
+                  ...register('acquisitionDate'),
+                }}
+              />
+            </FormControl>
+
+            <FormControl isInvalid={Boolean(errors.issuingAuthority)}>
+              <FormLabel
+                htmlFor="issuingAuthority"
+                w={'fit-content'}
+              >
+                수여 기관
+              </FormLabel>
+              <FormTextInput
+                placeholder="수여 기관을 입력해주세요."
+                id="issuingAuthority"
+                register={{
+                  ...register('issuingAuthority'),
+                }}
+              />
+            </FormControl>
+          </Flex>
+          <FormControl isInvalid={Boolean(errors.link)}>
+            <FormLabel
+              htmlFor="link"
+              w={'8.625rem'}
+            >
+              링크
+            </FormLabel>
+            <FormTextInput
+              placeholder="https://"
+              id="link"
+              register={{ ...register('link') }}
+            />
+          </FormControl>
+
+          <FormControl isInvalid={Boolean(errors.description)}>
+            <FormLabel
+              htmlFor="description"
+              w={'8.625rem'}
+            >
+              설명
+            </FormLabel>
+            <FormTextarea
+              h={'16.625rem'}
+              placeholder="내용을 입력해주세요."
+              id="description"
+              register={{ ...register('description') }}
+              errors={errors}
+              autoComplete="off"
+              spellCheck="false"
+              resize="none"
+            />
+          </FormControl>
+          <HStack
+            justifyContent={'center'}
+            w={'full'}
+            spacing={'1.5rem'}
+          >
+            <Button
+              size={'sm'}
+              type="submit"
+            >
+              저장
+            </Button>
+            <Button
+              size={'sm'}
+              variant={'cancel'}
+            >
+              취소
+            </Button>
+          </HStack>
+        </Flex>
+      </form>
+    </BorderBox>
+  );
+};
+
+export default AwardForm;
