c849f6ada31f1922bd26695fe3c7b2a30527231e
이민희 <mhl98@dgu.ac.kr>
2023-11-06 13:26:34 +0900

    feat: 이력서 어학 get, post api 작성 (+낙관적업데이트)

---
+++ src/queries/resume/create/usePostResumeLanguage.ts
@@ -0,0 +1,27 @@
+import { useMutation } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
+import { postResumeLanguage } from '~/api/resume/create/postResumeLanguage';
+import { Language } from '~/types/language';
+
+export const usePostResumeLanguage = () => {
+  const queryClient = useQueryClient();
+  const TARGET_QUERY_KEY = 'getResumeLanguage';
+  return useMutation({
+    mutationKey: ['postLanguage'],
+    mutationFn: postResumeLanguage,
+    onMutate: async (newLanguage) => {
+      await queryClient.cancelQueries({ queryKey: [TARGET_QUERY_KEY] });
+      const previousLanguages = queryClient.getQueryData([TARGET_QUERY_KEY]);
+      queryClient.setQueryData([TARGET_QUERY_KEY], (old: Language[]) => [...old, newLanguage]);
+      return { previousLanguages };
+    },
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    onError: (err, newLanguage, context) => {
+      queryClient.setQueryData([TARGET_QUERY_KEY], context?.previousLanguages);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [TARGET_QUERY_KEY] });
+    },
+  });
+};
