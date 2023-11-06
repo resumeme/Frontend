5e204202f1969af2e851e4e233021522cc6c24c4
이민희 <mhl98@dgu.ac.kr>
2023-11-06 14:34:24 +0900

    feat: 업무경험 상세 데이터 뿌려주기

--- src/components/organisms/ResumeDetails/CareerDetails.tsx
+++ src/components/organisms/ResumeDetails/CareerDetails.tsx
@@ -1,3 +1,4 @@
+import { Divider } from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
 import { BorderBox } from '~/components/atoms/BorderBox';
 import Career from '~/types/career';
@@ -15,6 +16,21 @@ const CareerDetails = ({ data }: { data: Career[] }) => {
             w={'100%'}
           >
             <div>{company.companyName}</div>
+            <div>{company.position}</div>
+            <div>{company.skills}</div>
+            <Divider />
+            {company.duties?.map(({ title, startDate, endDate, description }) => (
+              <div key={uuidv4()}>
+                <span>{title}</span>
+                <span>{startDate}</span>
+                <span>{endDate}</span>
+                <span>{description}</span>
+              </div>
+            ))}
+
+            <div>{company.careerStartDate}</div>
+            {company.isCurrentlyEmployed && <div>{company.endDate}</div>}
+            <div>{company.careerContent}</div>
           </BorderBox>
         );
       })}
