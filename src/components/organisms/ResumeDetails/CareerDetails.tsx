e9699580cd674d19102dcbc552966a70a933b75a
이민희 <mhl98@dgu.ac.kr>
2023-11-06 11:04:41 +0900

    chore: 업무경험 get 주석 처리 (서버 오류 이슈)

--- src/components/organisms/ResumeDetails/CareerDetails.tsx
+++ src/components/organisms/ResumeDetails/CareerDetails.tsx
@@ -1,27 +1,29 @@
-import { useParams } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import { BorderBox } from '~/components/atoms/BorderBox';
-import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
-import Career from '~/types/career';
+// import { useParams } from 'react-router-dom';
+// import { v4 as uuidv4 } from 'uuid';
+// import { BorderBox } from '~/components/atoms/BorderBox';
+// import { useGetResumeCareer } from '~/queries/resume/details/useGetResumeCareer';
+// import Career from '~/types/career';
 
 const CareerDetail = () => {
-  const { id: resumeId } = useParams() as { id: string };
-
-  const { data } = useGetResumeCareer({ resumeId });
-  if (!data) {
-    return;
-  }
-  return (
-    <>
-      {data?.map((company: Career) => {
-        return (
-          <BorderBox key={uuidv4()}>
-            <div>{company.companyName}</div>
-          </BorderBox>
-        );
-      })}
-    </>
-  );
+  /**TODO - api 서버 오류 해결 후 주석 풀 것 */
+  // const { id: resumeId } = useParams() as { id: string };
+  // const { data } = useGetResumeCareer({ resumeId });
+  // if (!data) {
+  //   return;
+  // }
+  // return (
+  //   <>
+  //     {data?.map((company: Career) => {
+  //       return (
+  //         <BorderBox key={uuidv4()}>
+  //           <div>{company.companyName}</div>
+  //         </BorderBox>
+  //       );
+  //     })}
+  //   </>
+  // );
+  /**TODO - api 서버 오류 해결 후 삭제할 것 */
+  return <></>;
 };
 
 export default CareerDetail;
--- src/queries/resume/create/usePostResumeCareer.ts
+++ src/queries/resume/create/usePostResumeCareer.ts
@@ -1,27 +1,28 @@
 import { useMutation } from '@tanstack/react-query';
-import { useQueryClient } from '@tanstack/react-query';
+// import { useQueryClient } from '@tanstack/react-query';
 import { postResumeCareer } from '~/api/resume/create/postResumeCareer';
-import Career from '~/types/career';
+// import Career from '~/types/career';
 
 export const usePostResumeCareer = () => {
-  const queryClient = useQueryClient();
-  const TARGET_QUERY_KEY = 'getResumeCareer';
+  /**TODO - api 서버 오류 해결 후 주석 풀 것 */
+  // const queryClient = useQueryClient();
+  // const TARGET_QUERY_KEY = 'getResumeCareer';
   return useMutation({
     mutationKey: ['postCareer'],
     mutationFn: postResumeCareer,
-    onMutate: async (newCareer) => {
-      await queryClient.cancelQueries({ queryKey: [TARGET_QUERY_KEY] });
-      const previousCareers = queryClient.getQueryData([TARGET_QUERY_KEY]);
-      queryClient.setQueryData([TARGET_QUERY_KEY], (old: Career[]) => [...old, newCareer]);
-      return { previousCareers };
-    },
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    onError: (err, newCareer, context) => {
-      queryClient.setQueryData([TARGET_QUERY_KEY], context?.previousCareers);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: [TARGET_QUERY_KEY] });
-    },
+    //   onMutate: async (newCareer) => {
+    //     await queryClient.cancelQueries({ queryKey: [TARGET_QUERY_KEY] });
+    //     const previousCareers = queryClient.getQueryData([TARGET_QUERY_KEY]);
+    //     queryClient.setQueryData([TARGET_QUERY_KEY], (old: Career[]) => [...old, newCareer]);
+    //     return { previousCareers };
+    //   },
+    //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //   // @ts-ignore
+    //   onError: (err, newCareer, context) => {
+    //     queryClient.setQueryData([TARGET_QUERY_KEY], context?.previousCareers);
+    //   },
+    //   onSettled: () => {
+    //     queryClient.invalidateQueries({ queryKey: [TARGET_QUERY_KEY] });
+    //   },
   });
 };
