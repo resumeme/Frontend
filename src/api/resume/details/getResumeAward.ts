8750816596bcff8c6075716ab8a5a68895770a48
backward99 <wjeeho@naver.com>
2023-11-06 14:24:47 +0900

    feat: 수상 및 경력 데이터 받아오기

---
+++ src/api/resume/details/getResumeAward.ts
@@ -0,0 +1,21 @@
+import { isAxiosError } from 'axios';
+import { resumeMeAxios } from '~/api/axios';
+import { ResumeMeErrorResponse } from '~/types/errorResponse';
+
+export type GetResumeAward = { resumeId: string };
+
+export const getResumeAward = async ({ resumeId }: GetResumeAward) => {
+  try {
+    const { data } = await resumeMeAxios.get(`/v1/resume/${resumeId}/awards`, {
+      headers: {
+        /**FIXME - 쿠키 등에 별도 저장된 토큰 가져오기 */
+        access: import.meta.env.VITE_TEMP_MENTEE_TOKEN,
+      },
+    });
+    return data;
+  } catch (e) {
+    if (isAxiosError<ResumeMeErrorResponse>(e)) {
+      throw new Error(e.response?.data.message);
+    }
+  }
+};
