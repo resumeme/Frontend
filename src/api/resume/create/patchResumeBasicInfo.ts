ecf5afa1b58908a8d790a2497625fc07a04beaeb
Dongho Shin <kakkiid@gmail.com>
2023-11-06 18:28:28 +0900

    feat: 이력서 기본정보 api 작성

---
+++ src/api/resume/create/patchResumeBasicInfo.ts
@@ -0,0 +1,22 @@
+import { isAxiosError } from 'axios';
+import { resumeMeAxios } from '~/api/axios';
+import { ResumeMeErrorResponse } from '~/types/errorResponse';
+import { BasicInfo } from '~/types/userInfo';
+
+type patchResumeBasicInfo = { resumeId: string; resumeBasicInfo: BasicInfo };
+
+export const patchResumeBasicInfo = async ({ resumeId, resumeBasicInfo }: patchResumeBasicInfo) => {
+  try {
+    const { data } = await resumeMeAxios.patch(`/v1/resumes/${resumeId}`, resumeBasicInfo, {
+      headers: {
+        /**FIXME - 쿠키 등에 별도 저장된 토큰 가져오기 */
+        access: import.meta.env.VITE_TEMP_MENTEE_TOKEN,
+      },
+    });
+    return data;
+  } catch (e) {
+    if (isAxiosError<ResumeMeErrorResponse>(e)) {
+      throw new Error(e.response?.data.message);
+    }
+  }
+};
--- src/types/userInfo.ts
+++ src/types/userInfo.ts
@@ -1,6 +1,6 @@
 export type BasicInfo = {
   position: string;
-  skillset: string[];
+  skills: string[];
   introduce: string;
 };
 
