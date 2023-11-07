48b725fcdfeae6dadd44969128709f7bc86c10d2
이민희 <mhl98@dgu.ac.kr>
2023-11-08 01:04:49 +0900

    fix: 소셜 로그인 버튼 button감싸고 onclick전달

--- src/components/molecules/OAuthSignInButton/OAuthSignInButton.tsx
+++ src/components/molecules/OAuthSignInButton/OAuthSignInButton.tsx
@@ -4,9 +4,10 @@ import { assets } from '~/config/assets';
 
 type OAuthSignInButtonProps = {
   oAuthPlatform: 'kakao';
+  onClick: () => void;
 };
 
-const OAuthSignInButton = ({ oAuthPlatform }: OAuthSignInButtonProps) => {
+const OAuthSignInButton = ({ oAuthPlatform, onClick }: OAuthSignInButtonProps) => {
   const PLATFORMS = {
     kakao: {
       svg: assets.kakaoLogoSvg,
@@ -15,21 +16,23 @@ const OAuthSignInButton = ({ oAuthPlatform }: OAuthSignInButtonProps) => {
   };
 
   return (
-    <BorderBox
-      w={'5.8rem'}
-      h={'8rem'}
-      display={'flex'}
-      flexDirection="column"
-      gap={'0.7rem'}
-      justifyContent={'center'}
-      alignItems={'center'}
-    >
-      <Image
-        src={PLATFORMS[oAuthPlatform].svg}
-        alt={PLATFORMS[oAuthPlatform].text}
-      />
-      <Box as="span">{PLATFORMS[oAuthPlatform].text}</Box>
-    </BorderBox>
+    <button onClick={onClick}>
+      <BorderBox
+        w={'5.8rem'}
+        h={'8rem'}
+        display={'flex'}
+        flexDirection="column"
+        gap={'0.7rem'}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Image
+          src={PLATFORMS[oAuthPlatform].svg}
+          alt={PLATFORMS[oAuthPlatform].text}
+        />
+        <Box as="span">{PLATFORMS[oAuthPlatform].text}</Box>
+      </BorderBox>
+    </button>
   );
 };
 
