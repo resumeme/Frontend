147c654d1e6ead9c1c7c7a295b7c6baed892e2ec
이민희 <mhl98@dgu.ac.kr>
2023-11-08 01:05:28 +0900

    feat: 로그인 UI 완성

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,20 +1,7 @@
-import { environments } from '~/config/environments';
+import { SignInTemplate } from '~/components/templates/SignInTemplate';
 
 const SignInPage = () => {
-  const kakaoSignInUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${environments.kakaoRestApiKeyEnv()}&redirect_uri=${environments.kakaoRedirectUriEnv()}&response_type=code`;
-  const handleKakaoClick = () => {
-    window.location.href = kakaoSignInUrl;
-  };
-  return (
-    <div>
-      <div>resume.me</div>
-      <div>이력,써에 가입하고 피드백을 받아보세요.</div>
-      <div>연동할 소셜 서비스 계정을 선택하세요.</div>
-      <button onClick={handleKakaoClick}>카카오 로그인</button>
-      <a href="#">계정을 잊으셨나요?</a>
-      <div>ⓒ 아몬드빼빼로</div>
-    </div>
-  );
+  return <SignInTemplate />;
 };
 
 export default SignInPage;
