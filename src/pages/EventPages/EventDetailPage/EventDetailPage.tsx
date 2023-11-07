8e29ffa23cbbdc7e724eeabdeb5c997f3276d6b5
backward99 <wjeeho@naver.com>
2023-11-07 23:18:13 +0900

    feat: 이벤트 정보 블럭

--- src/pages/EventPages/EventDetailPage/EventDetailPage.tsx
+++ src/pages/EventPages/EventDetailPage/EventDetailPage.tsx
@@ -1,34 +1,31 @@
+import { Flex } from '@chakra-ui/react';
 import MentorProfile from '~/components/molecules/MentorProfile/MentorProfile';
+import EventDetail from '~/components/organisms/EventDetail/EventDetail';
 import { useGetMentorDetail } from '~/queries/user/details/useGetMentorDetail';
+import { Position } from '~/types/position';
 
 const DUMMY_DATA = {
   event: {
     info: {
-      title: 'title',
-      content: 'content',
-      maximumAttendee: 3,
+      title: 'title_b21dc40438ed',
+      content: 'dlfjgrp',
+      maximumCount: 0,
+      currentApplicantCount: 0,
+      positions: ['FRONT'] as Position[],
+      timeInfo: {
+        openDateTime: '2023-11-07 17:48:51',
+        closeDateTime: '2023-11-07 17:48:51',
+        endDate: '2023-11-07 17:48:51',
+      },
     },
-    time: {
-      openDateTime: '2023-10-23T12:00:00',
-      closeDateTime: '2023-10-24T12:00:00',
-      endDate: '2023-10-30T23:00:00',
-    },
-    positions: ['FRONT', 'BACK'],
     resumes: [
       {
-        resumeId: 1,
-        menteeName: '백둥둥',
-        resumeTitle: 'title',
-        progressStatus: 'APPLY',
-      },
-      {
-        resumeId: 4,
-        menteeName: '백둥둥2',
-        resumeTitle: 'title',
-        progressStatus: 'APPLY',
+        resumeId: 0,
+        menteeName: 'menteeName_a5382713f0d2',
+        resumeTitle: 'resumeTitle_3f57dbe0891f',
+        progressStatus: 'progressStatus_ad4597f425d4',
       },
     ],
-    applicantCount: 3,
   },
 };
 
@@ -36,10 +33,16 @@ const EventDetailPage = () => {
   const { data: mentor } = useGetMentorDetail({ mentorId: '1' });
 
   return (
-    <MentorProfile
-      mentor={mentor}
-      event={DUMMY_DATA.event}
-    />
+    <Flex gap={'2rem'}>
+      <MentorProfile
+        mentor={mentor}
+        event={DUMMY_DATA.event}
+      />
+      <EventDetail
+        mentor={mentor}
+        event={DUMMY_DATA.event}
+      />
+    </Flex>
   );
 };
 
