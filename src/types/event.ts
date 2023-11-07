5a27ec92c7fd277ffb646d974d4a7c67783ed726
backward99 <86753969+backward99@users.noreply.github.com>
2023-11-08 00:26:26 +0900

    Gmmq 319 feat: mentor block UI 개발 (#61)
    * feat: Mentor 정보 읽기 타입 추가
    
    * feat: 이벤트 정보 읽기 타입 추가, 타입 정리
    
    * feat: 멘토 정보 컴포넌트
    
    * feat: 더미 데이터로 데이터 뿌리기
    
    * style: 최대 6줄 표시 이후 ...

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,14 +1,33 @@
-export type CreateEvent = {
-  info: {
-    title: string;
-    content: string;
-    maximumAttendee: number;
-  };
-  time: {
-    now: string;
-    openDateTime: string;
-    closeDateTime: string;
-    endDate: string;
-  };
+type CreateEvent = {
+  info: EventInfo;
+  time: EventTime;
   positions: string[];
 };
+
+type EventInfo = {
+  title: string;
+  content: string;
+  maximumAttendee: number;
+};
+
+type EventTime = {
+  now: string;
+  openDateTime: string;
+  closeDateTime: string;
+  endDate: string;
+};
+
+type EventResume = {
+  resumeId: number;
+  menteeName: string;
+  resumeTitle: string;
+  progressStatus: string;
+};
+
+type ReadEvent = {
+  resumes: EventResume[];
+  applicantCount: number;
+  time: Omit<EventTime, 'now'>;
+} & Omit<CreateEvent, 'time'>;
+
+export type { CreateEvent, ReadEvent };
