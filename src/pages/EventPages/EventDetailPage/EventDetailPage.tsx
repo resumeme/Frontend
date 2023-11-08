58fbf3449ab5b5db24eaae59e01ac70761feff65
backward99 <wjeeho@naver.com>
2023-11-08 14:26:49 +0900

    feat: 더미데이터 삭제 후 api 연결

--- src/pages/EventPages/EventDetailPage/EventDetailPage.tsx
+++ src/pages/EventPages/EventDetailPage/EventDetailPage.tsx
@@ -1,36 +1,12 @@
 import { Flex } from '@chakra-ui/react';
 import { MentorProfile } from '~/components/molecules/MentorProfile';
 import { EventDetail } from '~/components/organisms/EventDetail';
+import { useGetEventDetail } from '~/queries/event/details/useGetEventDetail';
 import { useGetMentorDetail } from '~/queries/user/details/useGetMentorDetail';
-import { Position } from '~/types/position';
-
-const DUMMY_DATA = {
-  event: {
-    info: {
-      title: 'title_b21dc40438ed',
-      content: 'dlfjgrp',
-      maximumCount: 0,
-      currentApplicantCount: 0,
-      positions: ['FRONT'] as Position[],
-      timeInfo: {
-        openDateTime: '2023-11-07 17:48:51',
-        closeDateTime: '2023-11-07 17:48:51',
-        endDate: '2023-11-07 17:48:51',
-      },
-    },
-    resumes: [
-      {
-        resumeId: 0,
-        menteeName: 'menteeName_a5382713f0d2',
-        resumeTitle: 'resumeTitle_3f57dbe0891f',
-        progressStatus: 'progressStatus_ad4597f425d4',
-      },
-    ],
-  },
-};
 
 const EventDetailPage = () => {
   const { data: mentor } = useGetMentorDetail({ mentorId: '1' });
+  const { data: event } = useGetEventDetail({ eventId: '1' });
 
   return (
     <Flex
@@ -39,11 +15,11 @@ const EventDetailPage = () => {
     >
       <MentorProfile
         mentor={mentor}
-        event={DUMMY_DATA.event}
+        event={event}
       />
       <EventDetail
         mentor={mentor}
-        event={DUMMY_DATA.event}
+        event={event}
       />
     </Flex>
   );
