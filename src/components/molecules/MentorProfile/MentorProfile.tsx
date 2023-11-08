aabcfbab2d70217fb73bc320ef816e1101065371
backward99 <86753969+backward99@users.noreply.github.com>
2023-11-09 01:45:09 +0900

    Gmmq 322 feat: event info api 연결 (#74)
    * style: 멘토 profile 가로 너비 적용
    
    * fix: getEventDetail 오타 수정
    
    * chore: 사용하지 않는 코드 제거
    
    * style: 모집 상태 UI 변경
    
    * feat: 이벤트 정보 받아오기 - react-query 연결
    
    * feat: 더미데이터 삭제 후 api 연결
    
    * chore: 해야할 일 주석추가

--- src/components/molecules/MentorProfile/MentorProfile.tsx
+++ src/components/molecules/MentorProfile/MentorProfile.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/react';
+import { Flex, Heading, Text, VStack } from '@chakra-ui/react';
 import { Avatar } from '~/components/atoms/Avatar';
 import { BorderBox } from '~/components/atoms/BorderBox';
 import { Button } from '~/components/atoms/Button';
@@ -14,60 +14,62 @@ const MentorProfile = ({ mentor, event }: MentorProfileProps) => {
   const { nickname, introduce, imageUrl } = mentor;
 
   return (
-    <Box maxW="14.25rem">
-      <VStack gap={'1.56rem'}>
-        <Avatar
-          size="lg"
-          name={nickname}
-          src={imageUrl}
-        />
+    <VStack
+      w={'full'}
+      maxW="14.25rem"
+      gap={'1.56rem'}
+    >
+      <Avatar
+        size="lg"
+        name={nickname}
+        src={imageUrl}
+      />
+      <Flex
+        w={'100%'}
+        justifyContent={'space-between'}
+      >
+        <Heading
+          fontSize={'20px'}
+          color={'gray.800'}
+        >
+          {nickname}
+        </Heading>
+      </Flex>
+      <Flex
+        minH={'12.44rem'}
+        direction={'column'}
+        justifyContent={'space-between'}
+      >
+        <BorderBox>
+          <Text
+            noOfLines={6}
+            overflow={'hidden'}
+            color={'gray.700'}
+          >
+            {introduce}
+          </Text>
+        </BorderBox>
         <Flex
           w={'100%'}
           justifyContent={'space-between'}
+          mt={'1.56rem'}
         >
-          <Heading
-            fontSize={'20px'}
+          <Text
+            as={'span'}
             color={'gray.800'}
           >
-            {nickname}
-          </Heading>
-        </Flex>
-        <Flex
-          minH={'12.44rem'}
-          direction={'column'}
-          justifyContent={'space-between'}
-        >
-          <BorderBox>
-            <Text
-              noOfLines={6}
-              overflow={'hidden'}
-              color={'gray.700'}
-            >
-              {introduce}
-            </Text>
-          </BorderBox>
-          <Flex
-            w={'100%'}
-            justifyContent={'space-between'}
-            mt={'1.56rem'}
+            인원
+          </Text>
+          <Text
+            as={'span'}
+            color={'gray.800'}
           >
-            <Text
-              as={'span'}
-              color={'gray.800'}
-            >
-              인원
-            </Text>
-            <Text
-              as={'span'}
-              color={'gray.800'}
-            >
-              {event.info.currentApplicantCount}/{event.info.maximumCount}
-            </Text>
-          </Flex>
+            {event.info.currentApplicantCount}/{event.info.maximumCount}
+          </Text>
         </Flex>
-        <Button size={'full'}>신청하기</Button>
-      </VStack>
-    </Box>
+      </Flex>
+      <Button size={'full'}>신청하기</Button>
+    </VStack>
   );
 };
 
