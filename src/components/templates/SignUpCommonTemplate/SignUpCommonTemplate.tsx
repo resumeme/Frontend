ec29dba86710f8ebecec035fee8f5c3c21248d4c
이민희 <mhl98@dgu.ac.kr>
2023-11-08 19:51:04 +0900

    style: divider 헤더에서 분리

--- src/components/templates/SignUpCommonTemplate/SignUpCommonTemplate.tsx
+++ src/components/templates/SignUpCommonTemplate/SignUpCommonTemplate.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, Flex, VStack, Box } from '@chakra-ui/react';
+import { Image, Text, Flex, VStack, Box, Divider } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { BorderBox } from '~/components/atoms/BorderBox';
 import { Button } from '~/components/atoms/Button';
@@ -78,6 +78,7 @@ const SignUpCommonTemplate = ({ onNext }: SignUpCommonTemplateProps) => {
           mainMessage={CONSTANTS.SIGNUP_HEADER_MESSAGE.MAIN}
           subMessage={CONSTANTS.SIGNUP_HEADER_MESSAGE.SUB}
         />
+        <Divider borderColor={'gray.300'} />
         <form onSubmit={handleSubmit(onSubmit)}>
           <VStack
             w={'21.875rem'}
