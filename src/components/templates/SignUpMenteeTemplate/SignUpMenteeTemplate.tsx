8b21a5ebf16ba4a3e316b2d9fce28a7d59d20a68
이민희 <mhl98@dgu.ac.kr>
2023-11-08 17:00:19 +0900

    feat: 가입 페이지 스텝별로 템플릿 분리

---
+++ src/components/templates/SignUpMenteeTemplate/SignUpMenteeTemplate.tsx
@@ -0,0 +1,5 @@
+const SignUpMenteeTemplate = () => {
+  return <></>;
+};
+
+export default SignUpMenteeTemplate;
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,7 +1,33 @@
-import SignUpCommonTemplate from '~/components/templates/SignUpCommonTemplate/SignUpCommonTemplate';
+import { useState } from 'react';
+import { SignUpCommonTemplate } from '~/components/templates/SignUpCommonTemplate';
+import { SignUpMenteeTemplate } from '~/components/templates/SignUpMenteeTemplate';
+import { SignUpMentorTemplate } from '~/components/templates/SignUpMentorTemplate';
+import { SignUpRole, SignUpMentee, SignUpMentor, SignUpCommon } from '~/types/signUp';
+
+export type Step = 'COMMON' | SignUpRole | 'COMPLETE';
 
 const SignUpPage = () => {
-  return <SignUpCommonTemplate />;
+  const [step, setStep] = useState<Step>('COMMON');
+  const [commonData, setCommonData] = useState<SignUpCommon<SignUpRole>>();
+  const [menteeData, setMenteeData] = useState<SignUpMentee>();
+  const [mentorData, setMentorData] = useState<SignUpMentor>();
+  /*FIXME - 사용하지 않는 변수 lint에러 방지용 로그 .. 삭제 필요! */
+  console.log(commonData, menteeData, setMenteeData, mentorData, setMentorData);
+  return (
+    <>
+      {step === 'COMMON' && (
+        <SignUpCommonTemplate
+          onNext={(data) => {
+            setStep(data.role);
+            setCommonData(data);
+          }}
+        />
+      )}
+      {/**TODO - onNext에 각각 menteeData, mentorData를 바디로 api 호출 (commonData 삽입해서) */}
+      {step === 'ROLE_PENDING' && <SignUpMentorTemplate />}
+      {step === 'ROLE_MENTEE' && <SignUpMenteeTemplate />}
+    </>
+  );
 };
 
 export default SignUpPage;
