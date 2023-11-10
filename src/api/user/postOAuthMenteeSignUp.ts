ee50c13b9b30aa3ccf03ac6a082eefa7dcb3f646
이민희 <mhl98@dgu.ac.kr>
2023-11-10 18:22:09 +0900

    fix: 멘티 가입 url 수정

--- src/api/user/postOAuthMenteeSignUp.ts
+++ src/api/user/postOAuthMenteeSignUp.ts
@@ -2,7 +2,7 @@ import { resumeMeAxios } from '../axios';
 import { SignUpMentee } from '~/types/signUp';
 
 const postOauthMenteeSignUp = async (body: SignUpMentee) => {
-  const { data } = await resumeMeAxios.post('/v1/mentors', body);
+  const { data } = await resumeMeAxios.post('/v1/mentees', body);
   return { data };
 };
 
