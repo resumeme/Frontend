7a299e2cc8d1eee432d90f83b8c7ebedef02290e
이민희 <mhl98@dgu.ac.kr>
2023-11-10 20:16:50 +0900

    fix: 사용하지 않는 mutation 제거

--- src/queries/usePostOAuthMentorSignUp.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import postOauthMentorSignUp from '~/api/user/postOAuthMentorSignUp';
-
-export const usePostOAuthMentorSignUp = () => {
-  return useMutation({
-    mutationKey: ['oAuthMentorSignUp'],
-    mutationFn: postOauthMentorSignUp,
-  });
-};
