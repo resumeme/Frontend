e2d8f48200600409b6fb70db5bec00146d3946d0
이민희 <mhl98@dgu.ac.kr>
2023-11-12 03:48:46 +0900

    chore: handleDeleteForm 관련 주석 추가

--- src/hooks/useHandleFormState.ts
+++ src/hooks/useHandleFormState.ts
@@ -8,10 +8,13 @@ export const useHandleFormState = <T extends FieldValues>(
 ) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [showForm, setShowForm] = useState(false);
+
+  // Confirm 모달의 작성 취소 확인 버튼 이벤트핸들러
   const handleDeleteForm = () => {
     reset();
     setShowForm(false);
   };
+
   const handleCancel = () => {
     if (isDirty) {
       onOpen();
@@ -19,5 +22,6 @@ export const useHandleFormState = <T extends FieldValues>(
       setShowForm(false);
     }
   };
+
   return { isOpen, onClose, showForm, handleCancel, setShowForm, handleDeleteForm };
 };
