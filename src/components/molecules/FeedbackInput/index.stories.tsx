1d656d843c66757618164307bf02aeaf8aef0fbd
Dongho Shin <kakkiid@gmail.com>
2023-11-13 01:03:41 +0900

    feat: FeedbackInput UI 구현 (#85)

---
+++ src/components/molecules/FeedbackInput/index.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta } from '@storybook/react';
+import { useState } from 'react';
+import FeedbackInput from './FeedbackInput';
+
+const meta = {
+  title: 'Resumeme/Components/FeedbackInput',
+  component: FeedbackInput,
+  tags: ['autodocs'],
+} satisfies Meta<typeof FeedbackInput>;
+
+export default meta;
+
+export const Default = () => {
+  const [value, setValue] = useState<string | undefined>('');
+
+  const handleChangeMarkdownValue = (newValue: string | undefined) => {
+    setValue(newValue);
+  };
+  const handleFeedbackSave = (value: string) => {
+    if (value) {
+      alert(`Saved markdown value: ${value}`);
+    }
+  };
+
+  return (
+    <div>
+      <FeedbackInput
+        value={value}
+        onChange={handleChangeMarkdownValue}
+        onSaveClick={handleFeedbackSave}
+      />
+    </div>
+  );
+};
