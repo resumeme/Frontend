05e4526a04586a8196cfdf0b3c6d354faf3c88da
이민희 <mhl98@dgu.ac.kr>
2023-11-13 23:25:36 +0900

    fix: 입력 데이터 유무 검증 생략, commonData 없으면 공통 폼으로 이동

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -55,30 +55,34 @@ const SignUpPage = () => {
       {step === 'ROLE_PENDING' && (
         <SignUpMentorTemplate
           onNext={(data) => {
-            if (commonData && data) {
-              signUpMutate(
-                { body: { ...data, requiredInfo: commonData, cacheKey }, role: 'ROLE_PENDING' },
-                {
-                  onSuccess: ({ accessToken, refreshToken }) =>
-                    signUpSuccessCallback(accessToken, refreshToken, 'ROLE_PENDING'),
-                },
-              );
+            if (!commonData) {
+              setStep('COMMON');
+              return;
             }
+            signUpMutate(
+              { body: { ...data, requiredInfo: commonData, cacheKey }, role: 'ROLE_PENDING' },
+              {
+                onSuccess: ({ accessToken, refreshToken }) =>
+                  signUpSuccessCallback(accessToken, refreshToken, 'ROLE_PENDING'),
+              },
+            );
           }}
         />
       )}
       {step === 'ROLE_MENTEE' && (
         <SignUpMenteeTemplate
           onNext={(data) => {
-            if (commonData && data) {
-              signUpMutate(
-                { body: { ...data, requiredInfo: commonData, cacheKey }, role: 'ROLE_MENTEE' },
-                {
-                  onSuccess: ({ accessToken, refreshToken }) =>
-                    signUpSuccessCallback(accessToken, refreshToken, 'ROLE_MENTEE'),
-                },
-              );
+            if (!commonData) {
+              setStep('COMMON');
+              return;
             }
+            signUpMutate(
+              { body: { ...data, requiredInfo: commonData, cacheKey }, role: 'ROLE_MENTEE' },
+              {
+                onSuccess: ({ accessToken, refreshToken }) =>
+                  signUpSuccessCallback(accessToken, refreshToken, 'ROLE_MENTEE'),
+              },
+            );
           }}
         />
       )}
