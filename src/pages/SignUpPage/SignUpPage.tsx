f514a6ff5c8aa681399103f96a23a1da2a77cb70
이민희 <mhl98@dgu.ac.kr>
2023-11-13 22:47:33 +0900

    fix: cacheKey없이 sign up 페이지 접속 시 로그인 페이지로 리다이렉트

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useToast } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { SignUpCommonTemplate } from '~/components/templates/SignUpCommonTemplate';
 import { SignUpCompleteTemplate } from '~/components/templates/SignUpCompleteTemplate';
 import { SignUpMenteeTemplate } from '~/components/templates/SignUpMenteeTemplate';
 import { SignUpMentorTemplate } from '~/components/templates/SignUpMentorTemplate';
+import { appPaths } from '~/config/paths';
 import useUser from '~/hooks/useUser';
 import { usePostOAuthSignUp } from '~/queries/usePostOAuthSignUp';
 import { useCacheKeyStore } from '~/stores/useCacheKeyStore';
@@ -26,6 +29,18 @@ const SignUpPage = () => {
       initialUser(accessToken, refreshToken);
     }
   };
+
+  const toast = useToast();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!cacheKey) {
+      toast({
+        description: '소셜 로그인을 먼저 해주세요.',
+      });
+      navigate(appPaths.signIn);
+      return;
+    }
+  }, [cacheKey, navigate, toast]);
 
   return (
     <>
