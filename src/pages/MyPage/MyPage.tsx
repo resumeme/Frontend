5b9b12b95740112828f4280c8196af054e0e9232
backward99 <wjeeho@naver.com>
2023-11-14 01:47:31 +0900

    feat: 마이페이지 너비 설정, 프로필 정보 보여주기

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,5 +1,19 @@
-const MenteeMyPage = () => {
-  return <></>;
+import { Box } from '@chakra-ui/react';
+import Profile from '~/components/organisms/Profile/Profile';
+import useUser from '~/hooks/useUser';
+
+const MyPage = () => {
+  const { user } = useUser();
+
+  return (
+    <Box
+      maxW={'44.25rem'}
+      w={'100%'}
+      mx={'auto'}
+    >
+      {user && <Profile user={user} />}
+    </Box>
+  );
 };
 
-export default MenteeMyPage;
+export default MyPage;
