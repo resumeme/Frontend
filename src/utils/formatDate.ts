c641a206026ed8bd62b143b86d4d62c5ba9d6b9d
Dongho Shin <kakkiid@gmail.com>
2023-11-15 18:23:08 +0900

    feat: formatDate 날짜 포매팅 함수 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,4 +2,32 @@ const formatDate = (date: string) => {
   return date.slice(0, 10).replace(/-/g, '. ');
 };
 
-export { formatDate };
+const formatKoreanDateWithMinutes = (date: string) => {
+  const [datePart, timePart] = date.split(' ');
+  const [year, month, day] = datePart.split('-');
+  const [hour, minute] = timePart.split(':');
+
+  const formattedDate = `${year}년 ${month}월 ${day}일`;
+  const period = +hour >= 12 ? '오후' : '오전';
+  const formattedHour = +hour % 12 === 0 ? 12 : +hour % 12;
+
+  const formattedTime = `${period} ${formattedHour}시 ${minute}분`;
+
+  return `${formattedDate} ${formattedTime}`;
+};
+
+const formatKoreanDate = (date: string) => {
+  const [datePart, timePart] = date.split(' ');
+  const [year, month, day] = datePart.split('-');
+  const [hour, minute, second] = timePart.split(':');
+
+  const formattedDate = `${year}년 ${month}월 ${day}일`;
+  const period = +hour >= 12 ? '오후' : '오전';
+  const formattedHour = +hour % 12 === 0 ? 12 : +hour % 12;
+
+  const formattedTime = `${period} ${formattedHour}시 ${minute}분 ${second}초`;
+
+  return `${formattedDate} ${formattedTime}`;
+};
+
+export { formatDate, formatKoreanDate, formatKoreanDateWithMinutes };
