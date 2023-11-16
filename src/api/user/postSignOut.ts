643701c5d18e59a45457a10eed19d2641d12e5f6
backward99 <wjeeho@naver.com>
2023-11-16 14:29:48 +0900

    fix: 로그아웃 할 때 엑세스 토큰만 보내기

--- src/api/user/postSignOut.ts
+++ src/api/user/postSignOut.ts
@@ -4,13 +4,11 @@ import { getCookie } from '~/utils/cookie';
 
 const postSignOut = async () => {
   const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
-  const refreshToken = getCookie(CONSTANTS.REFRESH_TOKEN_HEADER);
 
-  if (accessToken && refreshToken) {
+  if (accessToken) {
     return await resumeMeAxios.post(`/v1/logout`, {
       headers: {
         [CONSTANTS.ACCESS_TOKEN_HEADER]: accessToken,
-        [CONSTANTS.REFRESH_TOKEN_HEADER]: refreshToken,
       },
     });
   }
