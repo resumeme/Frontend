95ff200e8478e6cb8807e97d970beebf614b2f89
backward99 <wjeeho@naver.com>
2023-11-16 23:11:32 +0900

    fix: 헤딩 제거

--- src/components/templates/ResumeManagementTemplate/ResumeManagementTemplate.tsx
+++ src/components/templates/ResumeManagementTemplate/ResumeManagementTemplate.tsx
@@ -1,31 +1,52 @@
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import { Flex, Heading, Spacer, Text } from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
 import ResumeItem from '../../organisms/ResumeManagementItem/ResumeItem';
 import { BorderBox } from '~/components/atoms/BorderBox';
+import { Button } from '~/components/atoms/Button';
+import { usePostCreateResume } from '~/queries/resume/create/usePostCreateResume';
 import { ResumeWithEvents } from '~/types/event';
-
 type ResumeManagementTemplateProps = {
   resumes: ResumeWithEvents[];
 };
 
 const ResumeManagementTemplate = ({ resumes }: ResumeManagementTemplateProps) => {
+  const { mutate: createResume } = usePostCreateResume();
+
   return (
     <>
-      <Heading
-        fontSize={'1.75rem'}
-        color={'gray.800'}
-        fontWeight={700}
-      >
-        이력서 관리
-      </Heading>
-      <Heading
+      <Flex
         mt={'2.5rem'}
-        fontSize={'1.25rem'}
-        color={'gray.700'}
-        fontWeight={700}
+        alignItems={'end'}
       >
-        내 이력서
-      </Heading>
+        <Heading
+          fontSize={'1.25rem'}
+          color={'gray.700'}
+          fontWeight={700}
+        >
+          내 이력서
+        </Heading>
+        <Spacer />
+        {/* <Flex
+          alignItems={'center'}
+          gap={'0.25rem'}
+        >
+          <Icon
+            as={MdAdd}
+            color={'gray.500'}
+          /> */}
+        <Button
+          p={0}
+          w={'min-content'}
+          h={'min-content'}
+          bg={'gray.200'}
+          size={'md'}
+          color={'gray.500'}
+          onClick={() => createResume()}
+        >
+          새 이력서 작성
+        </Button>
+      </Flex>
+      {/* </Flex> */}
       <Flex
         mt={'1.25rem'}
         direction={'column'}
