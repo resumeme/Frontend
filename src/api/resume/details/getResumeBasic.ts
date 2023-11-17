ee9fbb20dfdcf57991d779cbc9f8a151283a53ef
Dongho Shin <kakkiid@gmail.com>
2023-11-18 03:55:17 +0900

    chore: postResumeBasicInfo를 patchResumeBasicInfo로 통일, api 수정

--- src/api/resume/details/getResumeBasic.ts
+++ src/api/resume/details/getResumeBasic.ts
@@ -1,31 +1,25 @@
-import { isAxiosError } from 'axios';
+// import { isAxiosError } from 'axios';
 import { resumeMeAxios } from '~/api/axios';
 import CONSTANTS from '~/constants';
-import { ResumeMeErrorResponse } from '~/types/errorResponse';
+import { BasicInfo } from '~/types/basicInfo';
+// import { ResumeMeErrorResponse } from '~/types/errorResponse';
 import { getCookie } from '~/utils/cookie';
 import { formatPhoneNumber } from '~/utils/formatPhoneNumber';
 
 export type GetResumeBasic = { resumeId: string };
 
-export const getResumeBasic = async ({ resumeId }: GetResumeBasic) => {
+export const getResumeBasic = async ({ resumeId }: GetResumeBasic): Promise<BasicInfo> => {
   const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
 
-  try {
-    const { data } = await resumeMeAxios.get(`/v1/resumes/${resumeId}/basic`, {
-      headers: {
-        /**FIXME - 쿠키 등에 별도 저장된 토큰 가져오기 */
-        Authorization: accessToken,
-      },
-    });
+  const { data } = await resumeMeAxios.get(`/v1/resumes/${resumeId}/basic`, {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
 
-    if (data.ownerInfo && data.ownerInfo.phoneNumber) {
-      data.ownerInfo.phoneNumber = formatPhoneNumber(data.ownerInfo.phoneNumber);
-    }
-
-    return data;
-  } catch (e) {
-    if (isAxiosError<ResumeMeErrorResponse>(e)) {
-      throw new Error(e.response?.data.message);
-    }
+  if (data.ownerInfo && data.ownerInfo.phoneNumber) {
+    data.ownerInfo.phoneNumber = formatPhoneNumber(data.ownerInfo.phoneNumber);
   }
+
+  return data;
 };
--- src/queries/resume/details/useGetResumeBasic.ts
+++ src/queries/resume/details/useGetResumeBasic.ts
@@ -1,11 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { categoryKeys } from '../categoryKeys.const';
 import { GetResumeBasic, getResumeBasic } from '~/api/resume/details/getResumeBasic';
 
 export const useGetResumeBasic = ({ resumeId }: GetResumeBasic) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: categoryKeys.basic(resumeId),
     queryFn: () => getResumeBasic({ resumeId }),
-    enabled: !!resumeId,
   });
 };
