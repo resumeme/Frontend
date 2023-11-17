6c50f744c0363cf60f802f36a4ff065ab3e654fc
backward99 <wjeeho@naver.com>
2023-11-17 16:34:48 +0900

    fix: 로그아웃 성공하면 홈 화면으로 이동

--- src/queries/usePostSignOut.ts
+++ src/queries/usePostSignOut.ts
@@ -1,15 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import postSignOut from './../api/user/postSignOut';
 import useUser from '~/hooks/useUser';
 
 export const usePostSignOut = () => {
   const { clearUser } = useUser();
+  const navigate = useNavigate();
 
   return useMutation({
     mutationKey: ['signOut'],
     mutationFn: postSignOut,
     onSuccess: () => {
       clearUser();
+      navigate('/');
     },
     onError: () => {
       alert('잠시 후 다시 시도해 주세요.');
