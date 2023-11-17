f6a7222ee17fda38c1eda21cc41b0c6c70ce95d2
backward99 <wjeeho@naver.com>
2023-11-17 18:52:13 +0900

    style: 아이콘 추가, badge padding 변경

--- src/components/organisms/Profile/UserDetail.tsx
+++ src/components/organisms/Profile/UserDetail.tsx
@@ -1,3 +1,4 @@
+import { PhoneIcon } from '@chakra-ui/icons';
 import { Flex, Heading, Text } from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
 import { Badge } from '~/components/atoms/Badge';
@@ -35,7 +36,6 @@ const UserDetail = ({
     ML_AI: 'ai',
     FULLSTACK: 'fullstack',
   };
-
   return (
     <Flex
       direction={'column'}
@@ -52,6 +52,9 @@ const UserDetail = ({
         )}
         {role && (
           <Badge
+            display={'flex'}
+            py={0}
+            alignItems={'center'}
             type={role}
             fontSize={'1rem'}
           >
@@ -61,6 +64,7 @@ const UserDetail = ({
         <Flex gap={'0.25rem'}>
           {experiencedPositions?.map((position) => (
             <Label
+              fontWeight={500}
               alignSelf={'center'}
               fontSize={'0.875rem'}
               key={uuidv4()}
@@ -70,6 +74,7 @@ const UserDetail = ({
 
           {interestedPositions?.map((position) => (
             <Label
+              fontWeight={500}
               alignSelf={'center'}
               fontSize={'0.875rem'}
               key={uuidv4()}
@@ -78,7 +83,15 @@ const UserDetail = ({
           ))}
         </Flex>
       </Flex>
-      {phoneNumber && <Text as={'span'}>{phoneNumber}</Text>}
+      {phoneNumber && (
+        <Flex
+          align={'center'}
+          gap={'1rem'}
+        >
+          <PhoneIcon />
+          <Text as={'span'}>{phoneNumber}</Text>
+        </Flex>
+      )}
       {introduce && <Text whiteSpace={'pre-line'}>{introduce}</Text>}
       {interestedFields && (
         <Flex
