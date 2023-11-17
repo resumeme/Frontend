31fd9d0827e04fc657251be578789728809b1187
backward99 <wjeeho@naver.com>
2023-11-18 00:58:34 +0900

    refactor: SubmitButtonGruop 버튼 크기 커스텀

--- src/components/molecules/SubmitButtonGroup/SubmitButtonGroup.tsx
+++ src/components/molecules/SubmitButtonGroup/SubmitButtonGroup.tsx
@@ -1,23 +1,24 @@
 import { HStack } from '@chakra-ui/react';
 import { Button } from '~/components/atoms/Button';
+import { ButtonProps } from '~/components/atoms/Button/Button';
 
 type SubmitButtonGroupProps = {
   onCancel: () => void;
-};
+} & Pick<ButtonProps, 'size'>;
 
-const SubmitButtonGroup = ({ onCancel }: SubmitButtonGroupProps) => {
+const SubmitButtonGroup = ({ size = 'sm', onCancel }: SubmitButtonGroupProps) => {
   return (
     <>
       <HStack>
         <Button
-          size={'sm'}
+          size={size}
           variant={'cancel'}
           onClick={onCancel}
         >
           취소
         </Button>
         <Button
-          size={'sm'}
+          size={size}
           type="submit"
         >
           저장
