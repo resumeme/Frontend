39e758630c666e5c8612134a357737b0cb9c6a6a
이민희 <mhl98@dgu.ac.kr>
2023-11-19 03:12:45 +0900

    chore: EventGridItem 불필요한 args 제거

--- src/components/molecules/EventGridItem/index.stories.tsx
+++ src/components/molecules/EventGridItem/index.stories.tsx
@@ -5,9 +5,6 @@ const meta = {
   title: 'Resumeme/Components/EventGridItem',
   tags: ['autodocs'],
   component: EventGridItem,
-  argTypes: {
-    categoryTitle: { control: 'text' },
-  },
 } satisfies Meta<typeof EventGridItem>;
 
 export default meta;
