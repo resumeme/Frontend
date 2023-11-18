283f3fc73a7a50ac8a0c26ce1fb42823ebba4be0
backward99 <wjeeho@naver.com>
2023-11-19 01:27:14 +0900

    chore: rebase 후 사용하지 않는 코드 제거

--- src/components/templates/EditProfileTemplate/EditMentorProfileTemplate.tsx
+++ src/components/templates/EditProfileTemplate/EditMentorProfileTemplate.tsx
@@ -239,19 +239,3 @@ const EditMentorProfileTemplate = () => {
 };
 
 export default EditMentorProfileTemplate;
-
-const CareerContentModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
-  return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      size={'lg'}
-    >
-      <Text whiteSpace={'pre-line'}>
-        {`현) ▵▵▵ 시니어 풀스택 개발자 재직 중
-          전) ◻◻◻ 프론트엔드 개발자 5년 근무
-          전) ⎔⎔⎔ 인턴 프론트엔드 개발자 2년 근무`}
-      </Text>
-    </Modal>
-  );
-};
