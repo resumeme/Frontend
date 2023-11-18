66de6da171eb68f07c8540aeccb114b5ff36adc8
이민희 <mhl98@dgu.ac.kr>
2023-11-18 21:32:07 +0900

    feat: 이벤트 신청 에러 메시지 토스트

--- src/queries/event/usePostEventApply.ts
+++ src/queries/event/usePostEventApply.ts
@@ -1,9 +1,25 @@
+import { useToast } from '@chakra-ui/react';
 import { useMutation } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
 import postEventApply from '~/api/event/postEventApply';
+import { ResumeMeErrorResponse } from '~/types/errorResponse';
 
 const usePostEventApply = () => {
+  const toast = useToast();
   return useMutation({
     mutationFn: postEventApply,
+    onSuccess: () => {
+      toast({
+        description: '이벤트 신청에 성공했습니다.',
+      });
+    },
+    onError: (error) => {
+      if (isAxiosError<ResumeMeErrorResponse>(error)) {
+        toast({
+          description: error.response?.data.message,
+        });
+      }
+    },
   });
 };
 
