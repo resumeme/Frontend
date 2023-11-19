5ac9fca23bf2d55f95176d61109e27d09999f92b
이민희 <mhl98@dgu.ac.kr>
2023-11-20 00:06:14 +0900

    fix: 이벤트 리스트 조회 api 토큰 추가

--- src/api/event/getEventList.ts
+++ src/api/event/getEventList.ts
@@ -1,7 +1,15 @@
 import { resumeMeAxios } from '../axios';
+import CONSTANTS from '~/constants';
 import { EventListItem } from '~/types/event/eventListItem';
+import { getCookie } from '~/utils/cookie';
 
 export const getEventList = async (): Promise<EventListItem[]> => {
-  const { data } = await resumeMeAxios.get(`/v1/events`);
+  const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
+
+  const { data } = await resumeMeAxios.get(`/v1/events`, {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
   return data;
 };
