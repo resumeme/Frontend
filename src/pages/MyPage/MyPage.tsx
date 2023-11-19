d254d090b6d24b69962df5fed759cc8623ec270d
backward99 <wjeeho@naver.com>
2023-11-19 13:05:04 +0900

    refactor: 이력서 관리 페이지에 템플릿 적용

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -10,53 +10,6 @@ const MyPage = () => {
   const { user } = useUser();
   const { data: events } = useGetManagementEvents({ role: user?.role, userId: user?.id || 0 });
 
-  const DUMY_DATA = [
-    {
-      resumeInfo: {
-        id: 1,
-        title: 'title1',
-        modifiedAt: '2023.06.23',
-      },
-      events: [
-        {
-          eventInfo: {
-            eventId: 1,
-            title: '제목',
-            endDate: '2023-11-15T09:36:59.665972804',
-            status: 'OPEN',
-            positions: ['BACK', 'FRONT'] as Position[],
-          },
-          mentorInfo: {
-            mentorId: 1,
-            nickname: '주승멘토',
-            imageUrl: 'profile.png',
-          },
-        },
-        {
-          eventInfo: {
-            eventId: 2,
-            title: '제목',
-            endDate: '2023-11-15T09:36:59.665972804',
-            status: 'OPEN',
-            positions: ['DEVOPS'] as Position[],
-          },
-          mentorInfo: {
-            mentorId: 2,
-            nickname: '기안멘토',
-            imageUrl: 'profile.png',
-          },
-        },
-      ],
-    },
-    {
-      resumeInfo: {
-        id: 1,
-        title: 'title1',
-      },
-      events: [],
-    },
-  ];
-
   return (
     <Box
       maxW={'44.25rem'}
@@ -64,11 +17,7 @@ const MyPage = () => {
       mx={'auto'}
     >
       {user && <Profile user={user} />}
-      {user?.role === 'mentor' ? (
-        events && <EventProfile events={events} />
-      ) : (
-        <ResumeManagementTemplate resumes={DUMY_DATA} />
-      )}
+      {user?.role === 'mentor' && events && <EventProfile events={events} />}
     </Box>
   );
 };
