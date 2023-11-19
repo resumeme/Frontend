7e7a6aa31f976cef5fa2b723c2a6c5d8de369856
이민희 <mhl98@dgu.ac.kr>
2023-11-19 12:49:26 +0900

    fix: env constants import 누락 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 import { ERROR_MESSAGES } from './errorMessage';
 import { POSITION } from './position';
+import { environments } from '~/config/environments';
 
 const CONSTANTS = {
   ERROR_MESSAGES,
