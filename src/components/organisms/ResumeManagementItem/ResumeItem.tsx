877da80bd4574dd551c3dc6a12dafba8850002f3
backward99 <wjeeho@naver.com>
2023-11-19 11:04:13 +0900

    feat: 이력서 관리 아이템 UI 변경

--- src/components/organisms/ResumeManagementItem/ResumeItem.tsx
+++ src/components/organisms/ResumeManagementItem/ResumeItem.tsx
@@ -1,21 +1,6 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-  Flex,
-  Icon,
-  Input,
-  Spacer,
-  Text,
-} from '@chakra-ui/react';
-import { BiCommentError } from 'react-icons/bi';
+import { Flex, Icon, Input, Spacer, Text } from '@chakra-ui/react';
 import { MdOutlineArticle } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import { ManagementPanel } from '~/components/molecules/ManagementPanel';
 import { OptionsButton } from '~/components/molecules/OptionsButton';
 import { Option } from '~/components/molecules/OptionsButton/OptionsButton';
 import { useDeleteResume } from '~/queries/resume/delete/useDeleteResume';
@@ -28,7 +13,6 @@ type ResumeItemProps = {
 
 const ResumeItem = ({
   resume: {
-    events,
     resumeInfo: { modifiedAt, id, title },
   },
 }: ResumeItemProps) => {
@@ -94,80 +78,6 @@ const ResumeItem = ({
           placeholder="이력서에 대한 간단한 메모를 남겨보세요. ex) 12월 25일 제출 전까지 피드백 받기"
         />
       </Flex>
-      <Accordion
-        mt={'2.37rem'}
-        w={'full'}
-        allowToggle
-      >
-        <AccordionItem
-          border={'none'}
-          p={0}
-          w={'full'}
-        >
-          <Flex>
-            <AccordionButton
-              w={'7rem'}
-              _hover={{ bg: 'none' }}
-              p={0}
-            >
-              <Box
-                w={'full'}
-                as="span"
-                flex={1}
-                textAlign={'left'}
-                fontSize={'0.875rem'}
-              >
-                {`신청이력서 ${events.length}건`}
-              </Box>
-              <AccordionIcon
-                marginLeft={'0.37rem'}
-                fontSize={'0.875rem'}
-              />
-            </AccordionButton>
-            <Spacer />
-          </Flex>
-          <AccordionPanel
-            mt={'1rem'}
-            p={0}
-          >
-            <Flex
-              direction={'column'}
-              gap={'1.37rem'}
-            >
-              {events.length ? (
-                events.map((event) => (
-                  <ManagementPanel
-                    key={uuidv4()}
-                    url={`/resume/${id}/comment`}
-                    icon={
-                      <Icon
-                        color={'highlight.900'}
-                        as={BiCommentError}
-                        w={'1.25rem'}
-                      />
-                    }
-                    name={event.mentorInfo.nickname}
-                    status={event.eventInfo.status}
-                    title={event.eventInfo.title}
-                    date={modifiedAt}
-                  />
-                ))
-              ) : (
-                <Flex
-                  borderRadius={'0.3125rem'}
-                  bg={'gray.200'}
-                  justifyContent={'center'}
-                  alignItems={'center'}
-                  w={'full'}
-                  h={'4.375rem'}
-                >
-                  <Text>이력서에 대한 피드백이 없어요.</Text>
-                </Flex>
-              )}
-            </Flex>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
     </>
   );
 };
