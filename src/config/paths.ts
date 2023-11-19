6396f83338963f2c207f3788d8083855fe432dcf
backward99 <86753969+backward99@users.noreply.github.com>
2023-11-19 15:12:16 +0900

    Gmmq 512 fix:header UI 변경 (#136)
    * feat: 멘토 정보 입력 템플릿 추가
    
    * chore: rebase 후 사용하지 않는 코드 제거
    
    * refactor: 메뉴에 텍스트를 포함할 수 있도록 변경
    
    * feat: 이력서 관리 페이지 라우팅 추가
    
    * feat: 로그인 한 상태 ui및 기능 변경
    
    * chore: TODO 추가
    
    * style: 아바타 sm사이즈 조정
    
    * refactor: appPath 활용

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -2,4 +2,8 @@ export const appPaths = {
   signIn: '/sign-in',
   main: '/',
   resumeCreate: () => '/resume/create',
+  managementResume: '/resume/management',
+  viewEvent: '/event/view',
+  myPage: (id?: number) => (id ? `/mypage/${id}` : '/'),
+  userEditInfo: `/user/edit-info`,
 };
---
+++ src/pages/ResumePages/ManagementResumePage/ManagementResumePage.tsx
@@ -0,0 +1,19 @@
+import { Box } from '@chakra-ui/react';
+// import { ResumeManagementTemplate } from '../../../components/templates/ResumeManagementTemplate';
+// import { useGetManagementResumes } from '~/queries/resume/details/useGetManagementResumes';
+
+const ManagementResumePage = () => {
+  // const { data: resumes } = useGetManagementResumes();
+
+  return (
+    <Box
+      maxW={'44.25rem'}
+      w={'100%'}
+      mx={'auto'}
+    >
+      {/* <ResumeManagementTemplate resumes={resumes} /> */}
+    </Box>
+  );
+};
+
+export default ManagementResumePage;
