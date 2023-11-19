5f05616a1e5af9a8fa00c0209c44a5a99aea7418
backward99 <wjeeho@naver.com>
2023-11-19 14:35:26 +0900

    feat: 첨삭 이벤트 참여 이력서 관련 타입 지정

---
+++ src/constants/status.ts
@@ -0,0 +1,10 @@
+// TODO api-docs에 맞는 상태로 변경
+const RESUME_STATUS = {
+  READY: '첨삭 이벤트 오픈 예약',
+  REOPEN: '재 신청 시작',
+  CLOSE: '이벤트 신청 마감',
+  OPEN: '첨삭 이벤트 오픈',
+  FINISH: '이벤트 종료',
+};
+
+export { RESUME_STATUS };
