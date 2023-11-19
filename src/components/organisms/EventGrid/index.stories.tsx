6fcf3c080ae2256e48b14cada70c3f280e75a5e0
이민희 <mhl98@dgu.ac.kr>
2023-11-20 02:13:52 +0900

    fix: 이벤트그리드 스토리북 더미데이터 작성

--- src/components/organisms/EventGrid/index.stories.tsx
+++ src/components/organisms/EventGrid/index.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta } from '@storybook/react';
 import { EventGrid } from '.';
-import { DUMMY_DATA } from './dummy';
+import { EventListItem } from '~/types/event/eventListItem';
+import { Position } from '~/types/position';
 
 const meta = {
   title: 'Resumeme/Components/EventGrid',
@@ -11,5 +12,74 @@ const meta = {
 export default meta;
 
 export const Default = () => {
-  return <EventGrid events={DUMMY_DATA} />;
+  return <EventGrid events={DUMMY} />;
 };
+
+const DUMMY: EventListItem[] = [
+  {
+    mentorInfo: {
+      mentorId: 1,
+      nickname: '큰돌',
+      imageUrl: 'https://i.pinimg.com/736x/4a/d7/8f/4ad78f5e3407a9912fd0862be6a68a5b.jpg',
+    },
+    info: {
+      id: 1,
+      title:
+        '프론트엔드, 백엔드 이력서 첨삭해드립니다. 이게 길어지면요. 어떻게 되냐면요. 이렇게 됩니다.',
+      content: '내용',
+      maximumCount: 3,
+      currentApplicantCount: 0,
+      positions: ['FRONT', 'BACK'] as Position[],
+      timeInfo: {
+        openDateTime: '2023-10-11T17:27:13.040Z',
+        closeDateTime: '2023-10-31T17:27:13.040Z',
+        endDate: '2023-12-01',
+      },
+      status: 'OPEN',
+    },
+  },
+  {
+    mentorInfo: {
+      mentorId: 1,
+      nickname: '큰돌',
+      imageUrl: 'https://i.pinimg.com/736x/4a/d7/8f/4ad78f5e3407a9912fd0862be6a68a5b.jpg',
+    },
+    info: {
+      id: 1,
+      title:
+        '프론트엔드, 백엔드 이력서 첨삭해드립니다. 이게 길어지면요. 어떻게 되냐면요. 이렇게 됩니다.',
+      content: '내용',
+      maximumCount: 3,
+      currentApplicantCount: 0,
+      positions: ['FRONT', 'BACK'] as Position[],
+      timeInfo: {
+        openDateTime: '2023-10-11T17:27:13.040Z',
+        closeDateTime: '2023-10-31T17:27:13.040Z',
+        endDate: '2023-12-01',
+      },
+      status: 'OPEN',
+    },
+  },
+  {
+    mentorInfo: {
+      mentorId: 1,
+      nickname: '큰돌',
+      imageUrl: 'https://i.pinimg.com/736x/4a/d7/8f/4ad78f5e3407a9912fd0862be6a68a5b.jpg',
+    },
+    info: {
+      id: 1,
+      title:
+        '프론트엔드, 백엔드 이력서 첨삭해드립니다. 이게 길어지면요. 어떻게 되냐면요. 이렇게 됩니다.',
+      content: '내용',
+      maximumCount: 3,
+      currentApplicantCount: 0,
+      positions: ['FRONT', 'BACK'] as Position[],
+      timeInfo: {
+        openDateTime: '2023-10-11T17:27:13.040Z',
+        closeDateTime: '2023-10-31T17:27:13.040Z',
+        endDate: '2023-12-01',
+      },
+      status: 'OPEN',
+    },
+  },
+];
