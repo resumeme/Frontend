6d6284d8df478a58043d34df57504be9e75ef760
backward99 <wjeeho@naver.com>
2023-11-20 02:55:22 +0900

    faet: 이력서 관리 페이지 api 연결

--- src/pages/ResumePages/ManagementResumePage/ManagementResumePage.tsx
+++ src/pages/ResumePages/ManagementResumePage/ManagementResumePage.tsx
@@ -3,58 +3,19 @@ import { useState } from 'react';
 import { ResumeManagementTemplate } from '../../../components/templates/ResumeManagementTemplate';
 import { ResumeNavigator } from '~/components/organisms/ResumeNavigator';
 import FeedbackManagementTemplate from '~/components/templates/FeedbackManagementTemplate/FeedbackManagementTemplate';
-import { Position } from '~/types/position';
-import { ResumeStatus } from '~/types/resume/status';
-// import { useGetManagementResumes } from '~/queries/resume/details/useGetManagementResumes';
+import useUser from '~/hooks/useUser';
+import { useGetFeedbackResumes } from '~/queries/resume/useGetFeedbackResumes';
+import { useGetMyResumes } from '~/queries/resume/useGetMyResumes';
 
 export type Step = 'MY_RESUME' | 'FEEDBACK';
 
 const ManagementResumePage = () => {
-  // const { data: resumes } = useGetManagementResumes();
+  const { user } = useUser();
 
   const [step, setStep] = useState<Step>('MY_RESUME');
 
-  const resumes = [
-    {
-      id: 1,
-      title: 'title1',
-      modifiedAt: '2023-11-18T15:22:57.223387127',
-      position: ['FRONT'] as Position[],
-    },
-    {
-      id: 2,
-      title: 'title2',
-      modifiedAt: '2023-11-18T15:22:57.223405031',
-      position: ['BACK'] as Position[],
-    },
-  ];
-
-  const feedbackResumes = [
-    {
-      resumeId: 1,
-      status: 'REOPEN' as ResumeStatus,
-      title: '제목',
-      mentorName: '김주승',
-      startDate: '2023-11-18T15:23:01.249679309',
-      endDate: '2023-11-18T16:23:01.249681292',
-    },
-    {
-      resumeId: 1,
-      status: 'OPEN' as ResumeStatus,
-      title: '제목',
-      mentorName: '김주승',
-      startDate: '2023-11-18T15:23:01.249679309',
-      endDate: '2023-11-18T16:23:01.249681292',
-    },
-    {
-      resumeId: 1,
-      status: 'CLOSE' as ResumeStatus,
-      title: '제목',
-      mentorName: '김주승',
-      startDate: '2023-11-18T15:23:01.249679309',
-      endDate: '2023-11-18T16:23:01.249681292',
-    },
-  ];
+  const { data: myResumes } = useGetMyResumes();
+  const { data: feedbackResumes } = useGetFeedbackResumes({ menteeId: user?.id || 0 });
 
   return (
     <Box
@@ -74,7 +35,7 @@ const ManagementResumePage = () => {
         pageStep={step}
         onStep={(step) => setStep(step)}
       />
-      {step === 'MY_RESUME' && <ResumeManagementTemplate resumes={resumes} />}
+      {step === 'MY_RESUME' && <ResumeManagementTemplate resumes={myResumes} />}
       {step === 'FEEDBACK' && <FeedbackManagementTemplate resumes={feedbackResumes} />}
     </Box>
   );
