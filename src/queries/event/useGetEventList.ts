390bfae30ffec2322e128e1e8d2f69db210e7377
이민희 <mhl98@dgu.ac.kr>
2023-11-21 04:56:19 +0900

    fix: 이벤트 조회 query key에 페이지 정보 반영

--- src/queries/event/useGetEventList.ts
+++ src/queries/event/useGetEventList.ts
@@ -1,10 +1,11 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getEventList } from '~/api/event/getEventList';
+import { eventKeys } from '~/queries/event/eventKeys.const';
 import { Pagination } from '~/types/pagination';
 
 export const useGetEventList = ({ page, size }: Pagination) => {
   return useSuspenseQuery({
-    queryKey: ['getEventList'],
+    queryKey: eventKeys.getPaginatedEvents(page, size),
     queryFn: () => getEventList({ page, size }),
   });
 };
