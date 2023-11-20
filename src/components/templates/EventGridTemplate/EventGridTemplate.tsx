32fd0bb8ff70aed882bf607258f808a6d34d465a
이민희 <mhl98@dgu.ac.kr>
2023-11-21 04:49:27 +0900

    feat: 페이지네이션 이벤트 리스트 페이지에 반영

--- src/components/templates/EventGridTemplate/EventGridTemplate.tsx
+++ src/components/templates/EventGridTemplate/EventGridTemplate.tsx
@@ -1,9 +1,13 @@
 import { Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Pagination } from '~/components/molecules/Pagination';
 import { EventGrid } from '~/components/organisms/EventGrid';
 import { useGetEventList } from '~/queries/event/useGetEventList';
 
 const EventGridTemplate = () => {
-  const { data } = useGetEventList();
+  const [page, setPage] = useState(1);
+  const size = 10;
+  const { data } = useGetEventList({ page, size });
   return (
     <>
       <Text
@@ -14,6 +18,12 @@ const EventGridTemplate = () => {
         진행 중인 첨삭 이벤트
       </Text>
       <EventGrid events={data.events} />
+      <Pagination
+        size={size}
+        page={page}
+        setPage={setPage}
+        total={data.pageData.pageData.totalElements}
+      />
     </>
   );
 };
--- src/queries/event/useGetEventList.ts
+++ src/queries/event/useGetEventList.ts
@@ -1,9 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getEventList } from '~/api/event/getEventList';
+import { Pagination } from '~/types/pagination';
 
-export const useGetEventList = () => {
+export const useGetEventList = ({ page, size }: Pagination) => {
   return useSuspenseQuery({
     queryKey: ['getEventList'],
-    queryFn: getEventList,
+    queryFn: () => getEventList({ page, size }),
   });
 };
