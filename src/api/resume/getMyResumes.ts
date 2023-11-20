ef83074eab4b6c363267ec16afa020b517de738b
backward99 <86753969+backward99@users.noreply.github.com>
2023-11-20 14:58:18 +0900

    Gmmq 448 feat: 이력서 관리 페이지 api 연결 작업 (#145)
    * fix: api axios header 수정
    
    * feat: 내 이력서 불러오기 api 생성
    
    * feat: 첨삭 내역 불러오기 api 생성
    
    * faet: 이력서 관리 페이지 api 연결

--- src/api/resume/getMyResumes.ts
+++ src/api/resume/getMyResumes.ts
@@ -1,10 +1,11 @@
 import { resumeMeAxios } from '~/api/axios';
 import CONSTANTS from '~/constants';
-import { ResumeListItem } from '~/types/resume/resumeListItem';
+import { MyResume } from '~/types/resume/resumeListItem';
 import { getCookie } from '~/utils/cookie';
 
-export const getMyResumes = async (): Promise<ResumeListItem[]> => {
+export const getMyResumes = async (): Promise<MyResume[]> => {
   const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
+
   const { data } = await resumeMeAxios.get(`/v1/resumes`, {
     headers: {
       Authorization: accessToken,
--- src/queries/resume/useGetMyResumes.ts
+++ src/queries/resume/useGetMyResumes.ts
@@ -1,9 +1,15 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getMyResumes } from '~/api/resume/getMyResumes';
 
+export const resumesKeys = {
+  all: ['resumes'] as const,
+  my: () => [...resumesKeys.all, 'myResumes'] as const,
+  feedback: (menteeId: number) => [...resumesKeys.all, menteeId],
+};
+
 export const useGetMyResumes = () => {
   return useSuspenseQuery({
-    queryKey: ['getMyResumes'],
+    queryKey: resumesKeys.my(),
     queryFn: getMyResumes,
   });
 };
