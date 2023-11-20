ba40b71d9dcc11b330686490fda661126efd8dd1
이민희 <mhl98@dgu.ac.kr>
2023-11-20 14:39:59 +0900

    fix: 이벤트 리스트 아이템 타입 수정

--- src/types/event/eventListItem.ts
+++ src/types/event/eventListItem.ts
@@ -1,10 +1,14 @@
 import { ReadEvent } from '~/types/event/event';
+import { PageData } from '~/types/pageData';
 
 export type EventListItem = {
-  info: ReadEvent;
-  mentorInfo: {
-    mentorId: number;
-    nickname: string;
-    imageUrl: string;
-  };
+  events: {
+    info: ReadEvent;
+    mentorInfo: {
+      mentorId: number;
+      nickname: string;
+      imageUrl: string;
+    };
+  }[];
+  pageData: PageData;
 };
