aec6c794624449aa9c0cf7660dfb6dc900b5e74b
Dongho Shin <kakkiid@gmail.com>
2023-11-22 03:49:56 +0900

    fix: 이력서 첨삭 코멘트 value에서 undefined 제거

--- src/components/templates/FeedbackResumeTemplate/FeedbackBlock.tsx
+++ src/components/templates/FeedbackResumeTemplate/FeedbackBlock.tsx
@@ -1,38 +1,51 @@
 import { PlusSquareIcon } from '@chakra-ui/icons';
 import { Box, Tooltip, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { FeedbackInput } from '~/components/molecules/FeedbackInput';
+import usePostFeedbackComment from '~/queries/event/usePostFeedbackComment';
 
 type FeedbackBlockProps = {
-  blockId?: string | undefined;
+  blockId: string;
 };
 
 const LABEL_TEXT = '첨삭하기';
 
 const FeedbackBlock = ({ blockId }: FeedbackBlockProps) => {
+  const { mutate } = usePostFeedbackComment();
+  const { eventId = '', resumeId = '' } = useParams();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [value, setValue] = useState<string | undefined>('');
+  const [value, setValue] = useState<string>('');
 
-  const handleChange = (newValue: string | undefined) => {
+  const handleChange = (newValue: string) => {
     if (newValue) {
       setValue(newValue);
     }
   };
 
   const handleSave = () => {
-    if (blockId) {
-      alert(`저장될 componentId: ${blockId} / ${value}`);
-    } else {
-      alert(`저장 되는 척 : ${value}`);
-    }
-    onClose(); // onSuccess일때만. 아닐 경우 오류만 띄운다.
     /* TODO POST API 연동
       - value를 가지고 저장함
       - 연동에 성공할 경우!
-        - 첨삭 코멘트 렌더링 하는 곳의 쿼리 데이터 변경하기 (최신화)
-        - value reset하기
-        - isOpen false로 변경하기 (에디터 안보이게하기) - onClose()
+        - 첨삭 코멘트 렌더링 하는 곳의 쿼리 데이터 변경하기 (최신화) ❌
+        - value reset하기 ⭕
+        - isOpen false로 변경하기 (에디터 안보이게하기) - onClose() ⭕
     */
+
+    const body = {
+      componentId: blockId,
+      content: value,
+    };
+
+    mutate(
+      { eventId, resumeId, body },
+      {
+        onSuccess: () => {
+          onClose();
+          setValue('');
+        },
+      },
+    );
   };
 
   return (
