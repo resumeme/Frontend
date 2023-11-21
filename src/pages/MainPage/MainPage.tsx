a1eee48011dfb4e02a7208e45659692ec7e163dd
backward99 <wjeeho@naver.com>
2023-11-21 23:47:31 +0900

    feat: 메인 페이지 상단 UI 구성

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,29 +1,156 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Flex, Grid, Image, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import { Button } from '~/components/atoms/Button';
+import { Option } from '~/components/molecules/OptionsButton/OptionsButton';
+import { assets } from '~/config/assets';
+import { appPaths } from '~/config/paths';
 import useUser from '~/hooks/useUser';
 import { usePostCreateResume } from '~/queries/resume/create/usePostCreateResume';
+import { LAYOUT_SIZE } from '~/routes/layoutSize.const';
+import { Position } from '~/types/position';
 
 const MainPage = () => {
-  const resumeCreateMutation = usePostCreateResume();
   const { user } = useUser();
 
-  const handleResumeCreate = () => {
-    resumeCreateMutation.mutate();
+  const { mutate: createResume } = usePostCreateResume();
+
+  const navigate = useNavigate();
+
+  const mentorButton: Option[] = [
+    {
+      text: '이벤트 생성',
+      onClick: () => navigate(appPaths.eventCreate()),
+    },
+    {
+      text: '이벤트 관리',
+      onClick: () => navigate(appPaths.myPage()),
+    },
+  ];
+
+  const menteeButton: Option[] = [
+    {
+      text: '새 이력서 작성',
+      onClick: () => createResume(),
+    },
+    {
+      text: '이력서 관리',
+      onClick: () => navigate(appPaths.managementResume()),
+    },
+  ];
+
+  const POSITION_IMAGES: { [key in Position]: string } = {
+    FRONT: 'https://i.pinimg.com/564x/97/7c/ad/977cad5f391fe80dc7aa4a8194c30e9e.jpg',
+    BACK: 'https://i.pinimg.com/564x/97/7c/ad/977cad5f391fe80dc7aa4a8194c30e9e.jpg',
+    DEVOPS: 'https://i.pinimg.com/564x/97/7c/ad/977cad5f391fe80dc7aa4a8194c30e9e.jpg',
+    FULLSTACK: 'https://i.pinimg.com/564x/97/7c/ad/977cad5f391fe80dc7aa4a8194c30e9e.jpg',
+    ML_AI: 'https://i.pinimg.com/564x/97/7c/ad/977cad5f391fe80dc7aa4a8194c30e9e.jpg',
+    MOBILE: 'https://i.pinimg.com/564x/97/7c/ad/977cad5f391fe80dc7aa4a8194c30e9e.jpg',
   };
+
+  const button_info = user ? (user.role === 'mentor' ? mentorButton : menteeButton) : menteeButton;
 
   return (
     <>
-      {/**FIXME - 아래는 임시 이력서 작성 버튼, 추후 대체할 것 */}
-      <Button
-        size={'md'}
-        onClick={handleResumeCreate}
+      <Box
+        bg={'white'}
+        mt={LAYOUT_SIZE.HEADER_HEIGHT}
       >
-        새 이력서 작성
-      </Button>
-      <Text>{user?.careerContent}</Text>
-      <Text>{user?.careerYear}</Text>
-      <Text>{user?.realName}</Text>
-      <Text>{user?.nickname}</Text>
+        <Flex
+          direction={{ base: 'column', md: 'row' }}
+          gap={{ base: '3rem', md: '' }}
+          mx={'auto'}
+          justify={'space-between'}
+          align={'center'}
+          py={'4rem'}
+          maxW={'950px'}
+          flexShrink={1}
+        >
+          <Flex
+            direction={'column'}
+            flexShrink={0}
+          >
+            <Text
+              color={'gray.800'}
+              fontSize={'1.625rem'}
+              fontWeight={600}
+              whiteSpace={'pre-line'}
+            >
+              {`이력서를 작성하는 당신에게는, \n최대한의 많은 피드백이 필요합니다.`}
+            </Text>
+            <Flex mt={'1.5rem'}>
+              <Text
+                color={'primary.900'}
+                fontWeight={900}
+              >
+                이력, 써
+              </Text>
+              <Text color={'gry.800'}>
+                의 피드백 커뮤니티를 경험하고 커리어의 나침반을 찾으세요.
+              </Text>
+            </Flex>
+            <Flex
+              mt={'3rem'}
+              gap={'1.5rem'}
+            >
+              <Button
+                size={'md'}
+                onClick={button_info[0].onClick}
+              >
+                {button_info[0].text}
+              </Button>
+              <Button
+                bg={'initial'}
+                color={'primary.900'}
+                border={'1px'}
+                borderColor={'primary.900'}
+                size={'md'}
+                onClick={() => navigate(appPaths.managementResume())}
+              >
+                이력서 관리
+              </Button>
+            </Flex>
+          </Flex>
+
+          <Image
+            flexShrink={1}
+            h={'15rem'}
+            src={assets.resumesSvg}
+            alt=""
+          />
+        </Flex>
+        <Grid
+          maxW={'960px'}
+          mx={'auto'}
+          templateColumns={{
+            base: 'repeat(3, 1fr)',
+            md: 'repeat(6, 1fr)',
+          }}
+          gap={'2rem'}
+        >
+          {Object.keys(POSITION_IMAGES).map((IMAGE) => (
+            <Flex
+              w={'full'}
+              key={uuidv4()}
+              direction={'column'}
+              justify={'center'}
+              align={'center'}
+              gap={'0.5rem'}
+            >
+              <Image
+                borderRadius={'1.5rem'}
+                flexShrink={1}
+                w={'full'}
+                aspectRatio={4.5 / 3}
+                src={POSITION_IMAGES[IMAGE as Position]}
+                alt={IMAGE}
+              />
+              <Text>{IMAGE}</Text>
+            </Flex>
+          ))}
+        </Grid>
+        {/**FIXME - 아래는 임시 이력서 작성 버튼, 추후 대체할 것 */}
+      </Box>
     </>
   );
 };
