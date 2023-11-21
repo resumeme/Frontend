5bb65d0c652b3e2ef37d9a0ab5e26528b09a4147
이민희 <mhl98@dgu.ac.kr>
2023-11-21 23:26:10 +0900

    feat: 피드백 불러오기 api 작성

---
+++ src/api/resume/feedback/getResumeFeedbacks.ts
@@ -0,0 +1,23 @@
+import { resumeMeAxios } from '~/api/axios';
+import CONSTANTS from '~/constants';
+import { Feedback } from '~/types/resume/feedback';
+import { getCookie } from '~/utils/cookie';
+
+export type GetResumeFeedbacks = {
+  resumeId: string;
+  eventId: string;
+};
+
+export const getResumeFeedbacks = async ({
+  resumeId,
+  eventId,
+}: GetResumeFeedbacks): Promise<Feedback[]> => {
+  const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
+
+  const { data } = await resumeMeAxios.get(`/v1/events/${eventId}/resumes/${resumeId}/comments`, {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
+  return data;
+};
