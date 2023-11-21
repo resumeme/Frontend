9e04fbe0a19bc3c473a8a31f190e5f8d5d2c63c8
Dongho Shin <kakkiid@gmail.com>
2023-11-22 01:40:18 +0900

    fix: 리모트 패널(총평) CSS 수정

--- src/components/atoms/RemoteControl/RemoteControl.tsx
+++ src/components/atoms/RemoteControl/RemoteControl.tsx
@@ -1,34 +1,37 @@
-import { BorderBox } from '~/components/atoms/BorderBox';
+import { Box } from '@chakra-ui/react';
 import { BorderBoxProps } from '~/components/atoms/BorderBox/BorderBox';
 
 type RemoteControlProps = {
   top?: string;
-  left?: string;
   w?: string;
   children: React.ReactNode;
 } & BorderBoxProps;
 
 const RemoteControl = ({
   children,
-  top = '10%',
-  left = '2rem',
-  w = '17rem',
+  top = '10.6rem',
+  w = '15rem',
   ...props
 }: RemoteControlProps) => {
   return (
-    <BorderBox
+    <Box
       position="sticky"
+      zIndex={'1'}
       top={top}
-      left={left}
       w={w}
-      p={4}
-      minH={'5rem'}
-      zIndex={'sticky'}
-      hasShadow
+      py={3}
+      px={3}
+      minH={'17rem'}
+      h={'fit-content'}
+      bg={'white'}
+      border={'1px solid'}
+      borderColor={'gray.300'}
+      borderRadius={'2xl'}
+      boxShadow={'0px 0px 2px rgba(0, 0, 0, 0.25)'}
       {...props}
     >
       {children}
-    </BorderBox>
+    </Box>
   );
 };
 
