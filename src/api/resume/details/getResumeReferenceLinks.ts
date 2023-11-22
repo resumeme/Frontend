bb68dd336b7f336a4c5d44cdcd1b3146c0fbac45
이민희 <mhl98@dgu.ac.kr>
2023-11-22 23:24:54 +0900

    fix: 참고링크 조회 api 반영, 데이터 없는 경우 빈 배열로 반환

--- src/api/resume/details/getResumeReferenceLinks.ts
+++ src/api/resume/details/getResumeReferenceLinks.ts
@@ -1,25 +1,18 @@
-import { isAxiosError } from 'axios';
 import { resumeMeAxios } from '~/api/axios';
 import CONSTANTS from '~/constants';
-import { ResumeMeErrorResponse } from '~/types/errorResponse';
+import { ReadReferenceLink } from '~/types/referenceLink';
 import { getCookie } from '~/utils/cookie';
 
 export type GetResumeReferenceLinks = { resumeId: string };
 
-export const getResumeReferenceLinks = async ({ resumeId }: GetResumeReferenceLinks) => {
+export const getResumeReferenceLinks = async ({
+  resumeId,
+}: GetResumeReferenceLinks): Promise<ReadReferenceLink> => {
   const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
-
-  try {
-    const { data } = await resumeMeAxios.get(`/v1/resume/${resumeId}/link`, {
-      headers: {
-        /**FIXME - 쿠키 등에 별도 저장된 토큰 가져오기 */
-        Authorization: accessToken,
-      },
-    });
-    return data;
-  } catch (e) {
-    if (isAxiosError<ResumeMeErrorResponse>(e)) {
-      throw new Error(e.response?.data.message);
-    }
-  }
+  const { data } = await resumeMeAxios.get(`/v1/resumes/${resumeId}/links`, {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
+  return data.links ?? [];
 };
