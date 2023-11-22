96216633023ea4d46510db8fd637f9b870829d69
Dongho Shin <kakkiid@gmail.com>
2023-11-23 04:12:07 +0900

    feat: FeedbackView - 수정 및 삭제 기능 추가

--- src/components/molecules/FeedbackView/FeedbackView.tsx
+++ src/components/molecules/FeedbackView/FeedbackView.tsx
@@ -1,7 +1,13 @@
-import { Box, Flex, Text, Divider, Icon, Tooltip } from '@chakra-ui/react';
+import { Box, Flex, Text, Divider, Icon, Tooltip, useDisclosure } from '@chakra-ui/react';
 import MDEditor from '@uiw/react-md-editor';
+import { useState } from 'react';
 import { HiPencilAlt, HiTrash } from 'react-icons/hi';
+import { useParams } from 'react-router-dom';
+import { ConfirmModal } from '../ConfirmModal';
+import { FeedbackInput } from '../FeedbackInput';
 import { BorderBox } from '~/components/atoms/BorderBox';
+import useDeleteFeedbackComment from '~/queries/event/useDeleteFeedbackComment';
+import usePatchFeedbackComment from '~/queries/event/usePatchFeedbackComment';
 import { formatKoreanDateWithoutSeconds } from '~/utils/formatDate';
 import '@uiw/react-markdown-preview/markdown.css';
 
@@ -9,7 +15,6 @@ type FeedbackViewProps = {
   content?: string;
   lastModifiedAt?: string;
   commentId?: number;
-  componentId?: number;
   isAuthorizedMentor?: boolean;
 };
 
@@ -24,112 +29,157 @@ const FeedbackView = ({
 - **반갑습니다.**`,
   lastModifiedAt = '2023-11-15 17:17:09',
   commentId,
-  componentId,
   isAuthorizedMentor = false,
 }: FeedbackViewProps) => {
-  const handleRemove = () => {
-    /* TODO 코멘트 삭제 API */
-    alert(`commentId: ${commentId}`);
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [value, setValue] = useState<string | undefined>(content);
+  const { resumeId = '', eventId = '' } = useParams();
+  const { mutate: patchCommentMutate } = usePatchFeedbackComment(resumeId, eventId);
+  const { mutate: deleteCommentMutate } = useDeleteFeedbackComment(resumeId, eventId);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const isEditToggle = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleValueChange = (newValue: string | undefined) => {
+    if (newValue) {
+      setValue(newValue);
+    }
+  };
+
+  const handleModalOpen = () => {
+    onOpen();
   };
 
   const handleEdit = () => {
-    /* TODO 코멘트 수정 함수
-      - componentId, commentId로 FeedbackInput 컴포넌트 보여줌
-      - FeedbackView 컴포넌트 숨김
-    */
-    alert(`componentId: ${componentId}`);
+    if (value && commentId) {
+      const body = {
+        content: value,
+      };
+      patchCommentMutate(
+        { resumeId, eventId, commentId, body },
+        {
+          onSuccess: () => {
+            setValue('');
+            isEditToggle();
+          },
+        },
+      );
+    }
+  };
+
+  const handleRemove = () => {
+    if (resumeId && eventId && commentId) {
+      deleteCommentMutate({ resumeId, eventId, commentId });
+    }
   };
 
   const fomattedDate = formatKoreanDateWithoutSeconds(lastModifiedAt);
 
   return (
-    <BorderBox p={0}>
-      <Flex
-        justify={'space-between'}
-        borderWidth="1px"
-        borderTopRadius="1.06rem"
-        borderColor={'transparent'}
-        bg={'gray.200'}
-        px={5}
-        py={2}
-      >
-        <Flex align={'flex-end'}>
-          <Text
-            fontSize={'sm'}
-            fontWeight={'semibold'}
-          >
-            {fomattedDate}에 작성된 피드백
-          </Text>
-        </Flex>
-        {isAuthorizedMentor && (
+    <>
+      {isEdit ? (
+        <FeedbackInput
+          value={value}
+          onChange={handleValueChange}
+          onSaveClick={handleEdit}
+          onCancelClick={isEditToggle}
+        />
+      ) : (
+        <BorderBox p={0}>
           <Flex
-            gap={3}
-            align={'end'}
+            justify={'space-between'}
+            borderWidth="1px"
+            borderTopRadius="1.06rem"
+            borderColor={'transparent'}
+            bg={'gray.200'}
+            px={5}
+            py={2}
           >
-            <Box
-              as="button"
-              onClick={handleEdit}
-            >
-              <Tooltip
-                label={LABELS.EDIT}
-                placement="top"
-                hasArrow
+            <Flex align={'flex-end'}>
+              <Text
+                fontSize={'sm'}
+                fontWeight={'semibold'}
               >
-                <span>
-                  <Icon
-                    as={HiPencilAlt}
-                    alignSelf={'flex-end'}
-                  />
-                </span>
-              </Tooltip>
-            </Box>
-            <Box
-              as="button"
-              onClick={handleRemove}
-            >
-              <Tooltip
-                label={LABELS.REMOVE}
-                placement="top"
-                hasArrow
+                {fomattedDate}에 작성된 피드백
+              </Text>
+            </Flex>
+            {isAuthorizedMentor && (
+              <Flex
+                gap={3}
+                align={'end'}
               >
-                <span>
-                  <Icon as={HiTrash} />
-                </span>
-              </Tooltip>
-            </Box>
+                <Box
+                  as="button"
+                  onClick={isEditToggle}
+                >
+                  <Tooltip
+                    label={LABELS.EDIT}
+                    placement="top"
+                    hasArrow
+                  >
+                    <span>
+                      <Icon
+                        as={HiPencilAlt}
+                        alignSelf={'flex-end'}
+                      />
+                    </span>
+                  </Tooltip>
+                </Box>
+                <Box
+                  as="button"
+                  onClick={handleModalOpen}
+                >
+                  <Tooltip
+                    label={LABELS.REMOVE}
+                    placement="top"
+                    hasArrow
+                  >
+                    <span>
+                      <Icon as={HiTrash} />
+                    </span>
+                  </Tooltip>
+                </Box>
+              </Flex>
+            )}
           </Flex>
-        )}
-      </Flex>
-
-      <Divider />
-
-      <Flex
-        px={5}
-        py={3}
-        direction={'column'}
-      >
-        <Box
-          height={'full'}
-          width={'full'}
-          data-color-mode="light"
-          px={1}
-        >
-          <MDEditor.Markdown
-            source={content}
-            style={{ whiteSpace: 'pre-wrap' }}
-          />
-        </Box>
-        <Flex justify={'flex-end'}>
-          <Text
-            fontSize={'xs'}
-            fontWeight={'light'}
-            color={'gray.500'}
+          <Divider />
+          <Flex
+            px={5}
+            py={3}
+            direction={'column'}
           >
-            {fomattedDate}
-          </Text>
-        </Flex>
-      </Flex>
-    </BorderBox>
+            <Box
+              height={'full'}
+              width={'full'}
+              data-color-mode="light"
+              px={1}
+            >
+              <MDEditor.Markdown
+                source={content}
+                style={{ whiteSpace: 'pre-wrap' }}
+              />
+            </Box>
+            <Flex justify={'flex-end'}>
+              <Text
+                fontSize={'xs'}
+                fontWeight={'light'}
+                color={'gray.500'}
+              >
+                {fomattedDate}
+              </Text>
+            </Flex>
+          </Flex>
+          <ConfirmModal
+            isOpen={isOpen}
+            onClose={onClose}
+            message="정말로 삭제하시겠습니까?"
+            proceed={handleRemove}
+          />
+        </BorderBox>
+      )}
+    </>
   );
 };
 
