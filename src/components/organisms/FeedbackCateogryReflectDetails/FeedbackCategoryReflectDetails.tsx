563b3759022806bf7b5dae73a099341eb5d72339
이민희 <mhl98@dgu.ac.kr>
2023-11-24 00:54:56 +0900

    fix: snapshotData undefined 조건 추가

--- src/components/organisms/FeedbackCateogryReflectDetails/FeedbackCategoryReflectDetails.tsx
+++ src/components/organisms/FeedbackCateogryReflectDetails/FeedbackCategoryReflectDetails.tsx
@@ -30,6 +30,7 @@ const FeedbackCategoryReflectDetails = <T extends ReadCategories>({
   const [editTargetIndex, setEditTargetIndex] = useState<number | null>(null);
   const indexedComments = getIndexedCommentsObject(commentsData);
   const commentComponentIds = Object.keys(indexedComments).map((index) => parseInt(index));
+  if (!snapshotData) return;
   const indexedSnapshots = getIndexedSnapshotObject(snapshotData);
   return (
     <>
