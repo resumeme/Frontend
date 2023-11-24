beca4cf417ed758f1c350e43d8414d46c604587e
이민희 <mhl98@dgu.ac.kr>
2023-11-24 19:14:11 +0900

    fix: FeedbackView mentorData 필수값 반영

--- src/components/molecules/FeedbackView/index.stories.tsx
+++ src/components/molecules/FeedbackView/index.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta } from '@storybook/react';
 import FeedbackView from './FeedbackView';
+import { ReadMentor } from '~/types/mentor';
 
 const meta = {
   title: 'Resumeme/Components/FeedbackView',
@@ -9,10 +10,14 @@ const meta = {
 
 export default meta;
 
+const DUMMY_DATA = {
+  imageUrl: 'https://i.pinimg.com/564x/b9/6a/9f/b96a9f10dd4365c867ff8d8972864a01.jpg',
+  nickname: '내가멘토다',
+} as ReadMentor;
 export const Default = () => {
   return (
     <div>
-      <FeedbackView />
+      <FeedbackView mentorData={DUMMY_DATA} />
     </div>
   );
 };
