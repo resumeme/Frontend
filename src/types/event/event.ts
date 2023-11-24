29f270e320a22f25dfe7f68288bfd048df8cd3b4
backward99 <wjeeho@naver.com>
2023-11-25 01:24:02 +0900

    fix: post 타입 구분

--- src/types/event/event.ts
+++ src/types/event/event.ts
@@ -3,7 +3,7 @@ import { Position } from '../position';
 
 type CreateEvent = {
   info: EventInfo;
-  time: EventTime;
+  time: CreateEventTime;
   positions: Position[];
 };
 
@@ -15,10 +15,14 @@ type EventInfo = {
 
 type EventTime = {
   now: string;
-  openDateTime: string | null;
+  openDateTime: string;
   closeDateTime: string;
   endDate: string;
 };
+
+type CreateEventTime = {
+  openDateTime: string | null;
+} & Omit<EventTime, 'openDateTime'>;
 
 type EventResume = {
   resumeId: number;
@@ -56,4 +60,11 @@ type ReadManagementEvent = {
   resumes: EventResume[];
 };
 
-export type { CreateEvent, ReadEvent, EventResume, EventTime, ReadManagementEvent };
+export type {
+  CreateEvent,
+  ReadEvent,
+  EventResume,
+  EventTime,
+  ReadManagementEvent,
+  CreateEventTime,
+};
