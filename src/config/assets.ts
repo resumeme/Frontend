8c326166a6a3fe405598ea9024dbe53b0b9f0ac4
Dongho Shin <kakkiid@gmail.com>
2023-11-25 21:33:20 +0900

    style: logo svg 추가

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,13 +1,15 @@
 import resumesSvg from '~/assets/images/banner-illustration.svg';
 import kakaoLogoSvg from '~/assets/images/kakao-logo.svg';
+import logoSvg from '~/assets/images/logo.svg';
 import menteeSvg from '~/assets/images/mentee-outlined.svg';
 import mentorSvg from '~/assets/images/mentor-outlined.svg';
 import noDataSvg from '~/assets/images/no-data.svg';
 
 export const assets = {
+  logoSvg,
+  resumesSvg,
   kakaoLogoSvg,
-  noDataSvg,
   menteeSvg,
   mentorSvg,
-  resumesSvg,
+  noDataSvg,
 };
