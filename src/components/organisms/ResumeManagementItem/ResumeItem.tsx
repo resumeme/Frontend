1b8152de2115bfe7b53afb2ebac8c3eea31ee1c5
Dongho Shin <kakkiid@gmail.com>
2023-11-25 23:05:57 +0900

    feat: 이력서 메모 수정 기능 모달로 구현

--- src/components/organisms/ResumeManagementItem/ResumeItem.tsx
+++ src/components/organisms/ResumeManagementItem/ResumeItem.tsx
@@ -1,8 +1,15 @@
-import { Flex, Icon, Input, Spacer, Text } from '@chakra-ui/react';
+import { Box, Flex, Icon, Spacer, Text, useDisclosure } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { MdOutlineArticle } from 'react-icons/md';
 import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '~/components/atoms/Button';
+import { FormLabel } from '~/components/atoms/FormLabel';
+import { FormControl } from '~/components/molecules/FormControl';
+import { FormTextInput } from '~/components/molecules/FormTextInput';
+import { Modal } from '~/components/molecules/Modal';
 import { EditDeleteOptionsButton } from '~/components/molecules/OptionsButton';
 import { appPaths } from '~/config/paths';
+import { usePatchResumeMemo } from '~/queries/resume/create/usePatchResumeMemo';
 import { useDeleteResume } from '~/queries/resume/delete/useDeleteResume';
 import { MyResume } from '~/types/resume/resumeListItem';
 import { formatDate } from '~/utils/formatDate';
@@ -11,10 +18,16 @@ type ResumeItemProps = {
   resume: MyResume;
 };
 
-const ResumeItem = ({ resume: { id, modifiedAt, title } }: ResumeItemProps) => {
-  const navigate = useNavigate();
+type MemoFormType = {
+  memo: string;
+};
 
+const ResumeItem = ({ resume: { id, modifiedAt, title, memo } }: ResumeItemProps) => {
+  const navigate = useNavigate();
   const { mutate: deleteResume } = useDeleteResume();
+  const { mutate: patchMemo } = usePatchResumeMemo();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const HandleEdit = () => {
     navigate(appPaths.resumeEdit(id));
@@ -22,6 +35,68 @@ const ResumeItem = ({ resume: { id, modifiedAt, title } }: ResumeItemProps) => {
 
   const HandleDelete = () => {
     deleteResume({ resumeId: String(id) });
+  };
+
+  const MemoModal = () => {
+    const {
+      register,
+      handleSubmit,
+      getValues,
+      formState: { errors },
+    } = useForm<MemoFormType>();
+
+    const onSubmit: SubmitHandler<MemoFormType> = (body) => {
+      patchMemo({ resumeId: String(id), resumeMemo: body });
+      onClose();
+    };
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Flex
+            direction={'column'}
+            w="full"
+            gap={3}
+          >
+            <Flex
+              w={'full'}
+              direction={'column'}
+            >
+              <FormLabel>메모하기</FormLabel>
+              <FormControl isInvalid={Boolean(errors.memo)}>
+                <FormTextInput
+                  placeholder="40자 이내로 간략하게 작성해주세요."
+                  id="memo"
+                  fontSize={'sm'}
+                  color={'gray.700'}
+                  defaultValue={memo}
+                  register={{
+                    ...register('memo', {
+                      maxLength: {
+                        value: 41,
+                        message: `40자 이내로 입력해주세요. (${getValues('memo')?.length}/40자)`,
+                      },
+                    }),
+                  }}
+                  error={errors.memo}
+                />
+              </FormControl>
+            </Flex>
+            <Flex justify={'end'}>
+              <Button
+                size={'xs'}
+                type="submit"
+              >
+                저장
+              </Button>
+            </Flex>
+          </Flex>
+        </form>
+      </Modal>
+    );
   };
 
   return (
@@ -51,30 +126,41 @@ const ResumeItem = ({ resume: { id, modifiedAt, title } }: ResumeItemProps) => {
           {title}
         </Text>
       </Link>
-      <Flex
-        mt={'1.75rem'}
-        borderRadius={'0.3125rem'}
-        p={'0.75rem 1rem'}
-        bg={'gray.200'}
-        alignItems={'center'}
-        w={'full'}
-        gap={'0.69rem'}
+      <Box
+        cursor={'pointer'}
+        onClick={onOpen}
       >
-        <Icon
-          as={MdOutlineArticle}
-          color={'gray.500'}
-          w={'1.25rem'}
-        />
-        <Input
-          isTruncated
-          flexShrink={1}
-          h={'min-content'}
-          p={0}
-          m={0}
-          border={0}
-          placeholder="이력서에 대한 간단한 메모를 남겨보세요. ex) 12월 25일 제출 전까지 피드백 받기"
-        />
-      </Flex>
+        <Flex
+          mt={'1rem'}
+          borderRadius={'0.3125rem'}
+          p={'0.5rem 0.75rem'}
+          bg={'gray.200'}
+          alignItems={'center'}
+          w={'full'}
+          gap={'0.69rem'}
+        >
+          <Icon
+            as={MdOutlineArticle}
+            color={'gray.500'}
+            w={'1.25rem'}
+          />
+          <Text
+            isTruncated
+            flexShrink={1}
+            h={'min-content'}
+            p={0}
+            m={0}
+            border={0}
+            color={'gray.400'}
+            fontSize={'sm'}
+          >
+            {memo
+              ? memo
+              : '이력서에 대한 간단한 메모를 남겨보세요. ex. 12월 25일 제출 전까지 피드백 받기'}
+          </Text>
+        </Flex>
+      </Box>
+      <MemoModal />
     </>
   );
 };
