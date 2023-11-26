92878be68444e87eb146407de821618aba087352
backward99 <86753969+backward99@users.noreply.github.com>
2023-11-26 20:32:18 +0900

    Gmmq 715 fix: 이벤트 생성 시간 현재시간으로 포맷팅 (#209)
    * fix: 이벤트 생성 시간 현재시간으로 포맷팅
    
    * refactor: 더 간단한 방법으로 수정

--- src/api/event/postCreateEvent.ts
+++ src/api/event/postCreateEvent.ts
@@ -5,7 +5,12 @@ import { getCookie } from '~/utils/cookie';
 
 const postCreateEvent = async (data: CreateEvent): Promise<{ id: number }> => {
   const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
-  data.time.now = new Date().toISOString().substring(0, 16);
+
+  const offset = new Date().getTimezoneOffset() * 60000;
+
+  const formattedDate = new Date(Date.now() - offset).toISOString().substring(0, 16);
+
+  data.time.now = formattedDate;
   data.time.endDate = new Date(data.time.endDate).toISOString().substring(0, 16);
 
   const { data: eventId } = await resumeMeAxios.post(
