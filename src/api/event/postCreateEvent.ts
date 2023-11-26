1ee9ccf10f9b75df76278136b4d9205506a1e953
backward99 <wjeeho@naver.com>
2023-11-26 17:48:17 +0900

    fix: 이벤트 생성 시간 현재시간으로 포맷팅

--- src/api/event/postCreateEvent.ts
+++ src/api/event/postCreateEvent.ts
@@ -5,7 +5,17 @@ import { getCookie } from '~/utils/cookie';
 
 const postCreateEvent = async (data: CreateEvent): Promise<{ id: number }> => {
   const accessToken = getCookie(CONSTANTS.ACCESS_TOKEN_HEADER);
-  data.time.now = new Date().toISOString().substring(0, 16);
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}T${hour}:${minute}`;
+
+  data.time.now = formattedDate;
   data.time.endDate = new Date(data.time.endDate).toISOString().substring(0, 16);
 
   const { data: eventId } = await resumeMeAxios.post(
