5372cdb00600be8acb6fd7a575179f398afe402c
backward99 <86753969+backward99@users.noreply.github.com>
2023-11-27 15:16:15 +0900

    Gmmq 719 fix: 자기소개에 대한 입력이 없을 때 에러 수정 (#216)
    * fix: 자기소개에 대한 입력이 없을 때 에러 수정
    
    * fix: introduce가 없을 때 0으로 표시

--- src/components/templates/EditProfileTemplate/EditMenteeProfileTemplate.tsx
+++ src/components/templates/EditProfileTemplate/EditMenteeProfileTemplate.tsx
@@ -164,7 +164,9 @@ const EditMenteeProfileTemplate = () => {
                 ...register('introduce', {
                   maxLength: {
                     value: 100,
-                    message: `100자 이내로 입력해주세요. ${watch('introduce').length}자`,
+                    message: `100자 이내로 입력해주세요. ${
+                      watch('introduce') ? watch('introduce').length : 0
+                    }자`,
                   },
                 }),
               }}
