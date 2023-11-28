a4755c2ead37c46b71ac77333d7e63ba0ee6b318
backward99 <wjeeho@naver.com>
2023-11-28 10:38:29 +0900

    refactor: 공통에러 제외 에러는 발생한 곳에서 처리

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { createStandaloneToast } from '@chakra-ui/toast';
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
 import { RouterProvider } from 'react-router-dom';
-import { appPaths } from './config/paths';
 import CONSTANTS from './constants/index';
 import { ResumeMeErrorResponse } from './types/errorResponse';
 import router from '~/routes/router';
@@ -15,43 +14,11 @@ const { toast } = createStandaloneToast({ theme });
 const axiosErrorHandler = (error: Error) => {
   if (isAxiosError<ResumeMeErrorResponse>(error) && error.response) {
     const { code } = error.response.data;
-    const { status } = error.response;
 
     toast({
       description: CONSTANTS.ERROR_MESSAGES[code],
       status: 'error',
-      // position: 'top',
     });
-
-    setTimeout(() => {
-      switch (status) {
-        case 400:
-          switch (code) {
-            case 'BAD_REQUEST':
-              // window.location.replace(appPaths.main());
-              break;
-            case 'DUPLICATED_EVENT_OPEN':
-              // window.location.replace(appPaths.main());
-              break;
-
-            default:
-              break;
-          }
-          break;
-        case 401:
-          if (window.location.pathname === '/') {
-            window.location.assign(appPaths.signIn());
-          } else {
-            window.location.replace(appPaths.signIn());
-          }
-          break;
-        case 500:
-          // window.location.replace(appPaths.main());
-          break;
-        default:
-          break;
-      }
-    }, 800);
   }
   return;
 };
