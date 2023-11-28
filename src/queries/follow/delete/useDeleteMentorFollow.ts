98e7530ce36076d910a319be5052ce08e77e73f0
backward99 <wjeeho@naver.com>
2023-11-28 13:49:00 +0900

    feat: 팔로우, 팔로우 취소 후 상태 업데이트

--- src/queries/follow/delete/useDeleteMentorFollow.ts
+++ src/queries/follow/delete/useDeleteMentorFollow.ts
@@ -1,8 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { followKeys } from '../followKeys.const';
 import { deleteMentorFollow } from '~/api/follow/delete/deleteMentorFollow';
 
 export const useDeleteMentorFollow = () => {
+  const queryCLient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteMentorFollow,
+    onSuccess: () => {
+      queryCLient.refetchQueries({ queryKey: followKeys.all });
+    },
   });
 };
