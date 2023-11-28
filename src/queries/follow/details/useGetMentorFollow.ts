b39a7d7648c51a022fc26c9811298876debbf913
backward99 <wjeeho@naver.com>
2023-11-28 18:49:05 +0900

    fix: api 수정

--- src/queries/follow/details/useGetMentorFollow.ts
+++ src/queries/follow/details/useGetMentorFollow.ts
@@ -2,9 +2,9 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { followKeys } from '../followKeys.const';
 import { getMentorFollow } from '~/api/follow/details/getMentorFollow';
 
-export const useGetMentorFollow = () => {
+export const useGetMentorFollow = ({ mentorId }: { mentorId: number }) => {
   return useSuspenseQuery({
     queryKey: followKeys.all,
-    queryFn: getMentorFollow,
+    queryFn: () => getMentorFollow({ mentorId }),
   });
 };
