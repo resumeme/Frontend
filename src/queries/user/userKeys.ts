d36807754d17a2792c15910e416b7f41725b8ab7
이민희 <mhl98@dgu.ac.kr>
2023-11-29 15:15:19 +0900

    refactor: 유저 키 위치 이동

--- src/queries/user/userKeys.ts
+++ src/queries/user/userKeys.ts
@@ -1,4 +1,5 @@
 export const userKeys = {
   all: ['user'] as const,
+  user: () => [...userKeys.all, 'getUser'] as const,
   isAppliedEvent: (eventId: string) => [...userKeys.all, 'getIsAppliedEvent', { eventId }] as const,
 };
