bb54f32e5f6457a4f7bcebc48d6a186510502b33
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2023-11-30 02:29:19 +0900

    fix: 이벤트 상세 페이지 비회원일 때 팔로우, 신청 이력 관련 api 호출 막음 (#245)

--- src/queries/follow/details/useGetMentorFollow.ts
+++ src/queries/follow/details/useGetMentorFollow.ts
@@ -1,10 +1,12 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { followKeys } from '../followKeys.const';
 import { getMentorFollow } from '~/api/follow/details/getMentorFollow';
+import { UserRole } from '~/types/user';
 
-export const useGetMentorFollow = ({ mentorId }: { mentorId: number }) => {
-  return useSuspenseQuery({
+export const useGetMentorFollow = ({ mentorId, role }: { mentorId: number; role?: UserRole }) => {
+  return useQuery({
     queryKey: followKeys.all,
     queryFn: () => getMentorFollow({ mentorId }),
+    enabled: !!(role === 'mentee'),
   });
 };
