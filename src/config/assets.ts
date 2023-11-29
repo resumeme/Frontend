b19ba7392f4336889d8caf7d474e84d966b65d75
Dongho Shin <kakkiid@gmail.com>
2023-11-29 22:56:28 +0900

    feat: 직무 썸네일 이미지 추가

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -5,6 +5,12 @@ import logoSvg from '~/assets/images/logo.svg';
 import menteeSvg from '~/assets/images/mentee-outlined.svg';
 import mentorSvg from '~/assets/images/mentor-outlined.svg';
 import noDataSvg from '~/assets/images/no-data.svg';
+import backend from '~/assets/images/thumbnail/thumb-back.webp';
+import devops from '~/assets/images/thumbnail/thumb-devops.webp';
+import frontend from '~/assets/images/thumbnail/thumb-front.webp';
+import fullstack from '~/assets/images/thumbnail/thumb-fullstack.webp';
+import ml_ai from '~/assets/images/thumbnail/thumb-ml_ai.webp';
+import mobile from '~/assets/images/thumbnail/thumb-mobile.webp';
 
 export const assets = {
   logoSvg,
@@ -14,4 +20,12 @@ export const assets = {
   menteeSvg,
   mentorSvg,
   noDataSvg,
+  thumbnail: {
+    FRONT: frontend,
+    MOBILE: mobile,
+    FULLSTACK: fullstack,
+    BACK: backend,
+    ML_AI: ml_ai,
+    DEVOPS: devops,
+  },
 };
