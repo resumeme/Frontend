8e42c2261e7ea8bf220831bd3e1206dab7ae6b45
이민희 <mhl98@dgu.ac.kr>
2023-12-01 13:12:04 +0900

    feat: 이벤트 생성 로더 멘토 로그인 로직 추가

--- src/routes/EventCreateLoader.tsx
+++ src/routes/EventCreateLoader.tsx
@@ -8,10 +8,22 @@ const EventCreateLoader = () => {
   const toast = useToast();
   const { user } = useUser();
   const { data: myEvents } = useGetManagementEvents({ userId: Number(user?.id), role: user?.role });
-
   const hasOpenedEvent = myEvents?.find(
     (event) => event.info.status !== 'FINISH' && event.info.status !== 'CLOSE',
   );
+
+  if (!user) {
+    toast({ description: '로그인이 필요해요.', status: 'info' });
+    return <Navigate to={appPaths.signIn()} />;
+  }
+
+  if (user.role !== 'mentor') {
+    toast({
+      description: '멘토로 로그인해야 확인할 수 있어요.',
+      status: 'info',
+    });
+    return <Navigate to={appPaths.main()} />;
+  }
 
   if (hasOpenedEvent) {
     toast({ description: '한 번에 하나의 이벤트만 진행할 수 있어요.', status: 'info' });
