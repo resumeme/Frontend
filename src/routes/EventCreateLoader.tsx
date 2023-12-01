8d00d3fcbcdeb549c7b455dc5dab51f15a905f30
이민희 <mhl98@dgu.ac.kr>
2023-12-01 13:41:09 +0900

    fix: 유저 검사 로직 제거

--- src/routes/EventCreateLoader.tsx
+++ src/routes/EventCreateLoader.tsx
@@ -12,19 +12,6 @@ const EventCreateLoader = () => {
     (event) => event.info.status !== 'FINISH' && event.info.status !== 'CLOSE',
   );
 
-  if (!user) {
-    toast({ description: '로그인이 필요해요.', status: 'info' });
-    return <Navigate to={appPaths.signIn()} />;
-  }
-
-  if (user.role !== 'mentor') {
-    toast({
-      description: '멘토로 로그인해야 확인할 수 있어요.',
-      status: 'info',
-    });
-    return <Navigate to={appPaths.main()} />;
-  }
-
   if (hasOpenedEvent) {
     toast({ description: '한 번에 하나의 이벤트만 진행할 수 있어요.', status: 'info' });
     return <Navigate to={appPaths.main()} />;
