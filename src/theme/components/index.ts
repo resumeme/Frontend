a4f37be87d1768e205ff087025d36281fb1b4c7d
Dongho Shin <kakkiid@gmail.com>
2023-12-02 19:21:26 +0900

    style: 차크라 Checkbox Theming - 체크 색상 blue에서 primary로 변경

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,6 +1,7 @@
 import { Avatar } from './avatar';
 import { Button } from './button';
+import { Checkbox } from './checkbox';
 import { Input } from './input';
 import { Textarea } from './textarea';
 
-export { Input, Button, Avatar, Textarea };
+export { Input, Button, Avatar, Textarea, Checkbox };
