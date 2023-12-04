aad7a290ed57c827e4a3d0d37aa2002691b93c56
Dongho Shin <kakkiid@gmail.com>
2023-12-04 19:59:49 +0900

    feat: 빌드 중 favicon 생성  및 favicon 이미지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginFaviconsInject('./src/assets/favicon.png')],
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
   },
