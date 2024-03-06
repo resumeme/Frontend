acbf0643592c785073b57506d1bac649685bfca4
이민희 <mhl98@dgu.ac.kr>
2024-03-07 00:47:39 +0900

    refactor: 환경변수 유틸함수 수정

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,14 +1,13 @@
-const { VITE_BASE_URL, VITE_KAKAO_REST_API_KEY, VITE_KAKAO_REDIRECT_URI } = import.meta.env;
-
 export const environments = {
-  baseUrlEnv: () => checkEnv(VITE_BASE_URL),
-  kakaoRestApiKeyEnv: () => checkEnv(VITE_KAKAO_REST_API_KEY),
-  kakaoRedirectUriEnv: () => checkEnv(VITE_KAKAO_REDIRECT_URI),
+  baseUrlEnv: () => getEnvVariable('VITE_BASE_URL'),
+  kakaoRestApiKeyEnv: () => getEnvVariable('VITE_KAKAO_REST_API_KEY'),
+  kakaoRedirectUriEnv: () => getEnvVariable('VITE_KAKAO_REDIRECT_URI'),
 };
 
-const checkEnv = (envKey: string) => {
-  if (!envKey) {
+const getEnvVariable = (envKey: string) => {
+  const envVariable = import.meta.env[envKey];
+  if (!envVariable) {
     throw new Error(`${envKey} 환경변수가 설정돼있지 않습니다.`);
   }
-  return envKey;
+  return envVariable;
 };
