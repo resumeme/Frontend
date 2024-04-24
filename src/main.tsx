655cbb690065aa80dec41c7563df7a6963ba7073
Dongho Shin <kakkiid@gmail.com>
2024-04-24 11:19:50 +0900

    refactor: MSW 설치 및 API 모킹 폴더 초기화 (#282)
    * refactor: msw 설치
    
    * refactor: msw 브라우저 환경 integration
    
    * Revert "refactor: msw 브라우저 환경 integration"
    
    This reverts commit 6b0ed2246b8c17a87b381c6f2a4d9a81bee90494.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+async function enableMocking() {
+  // 우리는 빌드 환경에서도 사용해야 하므로 주석처리
+  // if (import.meta.env.MODE !== 'development') {
+  //   return;
+  // }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+});
