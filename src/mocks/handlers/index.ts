04e582d1dcf5d243dde986ca0375bc8de453fb64
이민희 <mhl98@dgu.ac.kr>
2024-04-24 16:36:44 +0900

    refactor: mock index 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,5 @@
-import { handlers as eventHandlers } from './event';
-import { handlers as resumeHandlers } from './resume';
-import { handlers as userHandlers } from './user';
+import { eventHandlers } from './event';
+import { resumeHandlers } from './resume';
+import { userHandlers } from './user';
 
 export const handlers = [...eventHandlers, ...resumeHandlers, ...userHandlers];
---
+++ src/mocks/handlers/resume/index.ts
@@ -0,0 +1,3 @@
+import { handlers } from './resume';
+
+export { handlers as resumeHandlers };
