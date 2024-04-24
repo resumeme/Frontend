7e14f715da3735ebe7577c82c13dbbf076a3dbe8
이민희 <mhl98@dgu.ac.kr>
2024-04-24 21:18:59 +0900

    feat: 이력서 관련 데이터 가져오기 msw

--- src/mocks/handlers/resume/resume.ts
+++ src/mocks/handlers/resume/resume.ts
@@ -3,6 +3,40 @@ import { environments } from '~/config/environments';
 
 export const handlers = [
   http.post(`${environments.baseUrlEnv()}/v1/resumes`, () => {
+    return HttpResponse.json({ id: 0 });
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/basic`, () => {
+    return HttpResponse.json({
+      title: '이력서 제목',
+      position: 'FRONT',
+      skills: ['React', 'TypeScript'],
+      introduce: '',
+      ownerInfo: {
+        id: 0,
+        name: '손흥민',
+        phoneNumber: '01012345678',
+      },
+    });
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/links`, () => {
+    return HttpResponse.json();
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/careers`, () => {
+    return HttpResponse.json();
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/projects`, () => {
+    return HttpResponse.json();
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/activities`, () => {
+    return HttpResponse.json();
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/certifications`, () => {
+    return HttpResponse.json();
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/foreignLanguages`, () => {
+    return HttpResponse.json();
+  }),
+  http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/trainings`, () => {
     return HttpResponse.json();
   }),
 ];
