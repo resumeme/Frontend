34256a2f9087089fba443ae3e73a533f965a8c17
이민희 <mhl98@dgu.ac.kr>
2024-04-26 19:53:23 +0900

    feat: 참고링크 get, 기본정보 patch 핸들러 작성

--- src/mocks/handlers/resume/resume.ts
+++ src/mocks/handlers/resume/resume.ts
@@ -1,25 +1,28 @@
 import { HttpResponse, http } from 'msw';
 import { environments } from '~/config/environments';
 
+const referenceLinkMock = [
+  {
+    componentId: 0,
+    reflectFeedback: false,
+    createdDate: '2024-05-01T23:59:59.999Z',
+    linkType: 'GITHUB',
+    url: 'https://github.com/resumeme/Frontend',
+  },
+];
 export const handlers = [
   http.post(`${environments.baseUrlEnv()}/v1/resumes`, () => {
     return HttpResponse.json({ id: 0 });
   }),
   http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/basic`, () => {
-    return HttpResponse.json({
-      title: '이력서 제목',
-      position: 'FRONT',
-      skills: ['React', 'TypeScript'],
-      introduce: '',
-      ownerInfo: {
-        id: 0,
-        name: '손흥민',
-        phoneNumber: '01012345678',
-      },
-    });
+    return HttpResponse.json();
+  }),
+  // patchResumeBasicInfo
+  http.patch(`${environments.baseUrlEnv()}/v2/resumes/:id`, () => {
+    return HttpResponse.json();
   }),
   http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/links`, () => {
-    return HttpResponse.json();
+    return HttpResponse.json(referenceLinkMock);
   }),
   http.get(`${environments.baseUrlEnv()}/v1/resumes/:id/careers`, () => {
     return HttpResponse.json();
